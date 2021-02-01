bff3a3d699f2cc9df40c61f3028bd0898b3a670a
Ben R. Ryan <brryan@lanl.gov>
2021-02-01 15:17:24 -0700

    file

---
+++ src/pgen/pgen.cpp
@@ -0,0 +1,15 @@
+#include "pgen.hpp"
+
+namespace phoebus {
+
+/*std::map<std::string, std::function<void(MeshBlock*, ParameterInput*)>> GetProblems() {
+  std::map<std::string, std::function<void(MeshBlock*, ParameterInput*)>> problems;
+  problems["shocktube"] = YuckyOldWornOutProblemGenerator;
+}*/
+/*std::map<std::string, std::function<void(MeshBlock*, ParameterInput*)>> GetProblemGenerator(ParameterInput *pin) {
+  problems["shocktube"] = YuckyOldWornOutProblemGenerator;
+}*/
+
+
+
+}
