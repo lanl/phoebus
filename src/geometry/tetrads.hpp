eea85054f36eb35d6af31a4cf0ed4430c70e707d
Ben R. Ryan <brryan@lanl.gov>
2021-03-04 10:44:04 -0700

    Fixed the problem I guess

--- src/geometry/tetrads.hpp
+++ src/geometry/tetrads.hpp
@@ -7,22 +7,24 @@
 
 namespace Geometry {
 
-#define MULOOP for (int mu = 0; mu < NDFULL; mu++)
-#define NULOOP for (int nu = 0; nu < NDFULL; nu++)
+//#define MULOOP for (int mu = 0; mu < NDFULL; mu++)
+//#define SPACETIMELOOP(nu) for (int nu = 0; nu < NDFULL; nu++)
+
+#define SPACETIMELOOP(mu) for (int mu = 0; mu < NDFULL; mu++)
 
 class Tetrads {
 public:
   KOKKOS_FUNCTION
-  Tetrads(const double UCon[NDFULL], const double Trial[NDFULL],
-          const double GCov[NDFULL][NDFULL]) {
+  Tetrads(const double Ucon[NDFULL], const double Trial[NDFULL],
+          const double Gcov[NDFULL][NDFULL]) {
 
     Real X1ness = 0.;
     Real X2ness = 0.;
     Real X3ness = 0.;
-    MULOOP {
-      X1ness += GCov[1][mu] * Trial[mu] / sqrt(fabs(GCov[1][1]));
-      X2ness += GCov[2][mu] * Trial[mu] / sqrt(fabs(GCov[2][2]));
-      X3ness += GCov[3][mu] * Trial[mu] / sqrt(fabs(GCov[3][3]));
+    SPACETIMELOOP(mu) {
+      X1ness += Gcov[1][mu] * Trial[mu] / sqrt(fabs(Gcov[1][1]));
+      X2ness += Gcov[2][mu] * Trial[mu] / sqrt(fabs(Gcov[2][2]));
+      X3ness += Gcov[3][mu] * Trial[mu] / sqrt(fabs(Gcov[3][3]));
     }
     X1ness = fabs(X1ness);
     X2ness = fabs(X2ness);
@@ -30,126 +32,126 @@ public:
 
     // Normalize Trial vector
     double norm = 0.;
-    MULOOP NULOOP { norm += Trial[mu] * Trial[nu] * GCov[mu][nu]; }
+    SPACETIMELOOP(mu) SPACETIMELOOP(nu) { norm += Trial[mu] * Trial[nu] * Gcov[mu][nu]; }
 
     // Time component parallel to u^{\mu}
-    MULOOP { ECon_[0][mu] = UCon[mu]; }
+    SPACETIMELOOP(mu) { Econ_[0][mu] = Ucon[mu]; }
 
     // Use X1, X2, X3. Then, whichever is closest to Trial, overwrite.
-    MULOOP {
-      ECon_[1][mu] = Delta_(mu, 1);
-      ECon_[2][mu] = Delta_(mu, 2);
-      ECon_[3][mu] = Delta_(mu, 3);
+    SPACETIMELOOP(mu) {
+      Econ_[1][mu] = Delta_(mu, 1);
+      Econ_[2][mu] = Delta_(mu, 2);
+      Econ_[3][mu] = Delta_(mu, 3);
     }
 
     if (norm > SMALL) {
       // We can use the Trial vector
       if (X1ness > X2ness && X1ness > X3ness) {
         // Trial vector is closest to X1. Overwrite
-        MULOOP { ECon_[1][mu] = Trial[mu]; }
+        SPACETIMELOOP(mu) { Econ_[1][mu] = Trial[mu]; }
       } else if (X2ness >= X1ness && X2ness > X3ness) {
         // Trial vector is closest to X2. Overwrite
-        MULOOP { ECon_[2][mu] = Trial[mu]; }
+        SPACETIMELOOP(mu) { Econ_[2][mu] = Trial[mu]; }
       } else { // Trial vector is closest X3. Overwrite
-        MULOOP { ECon_[3][mu] = Trial[mu]; }
+        SPACETIMELOOP(mu) { Econ_[3][mu] = Trial[mu]; }
       }
     }
 
     // Gram-Schmidt and normalization
-    Normalize_(ECon_[0], GCov);
-    ProjectOut_(ECon_[1], ECon_[0], GCov);
-    Normalize_(ECon_[1], GCov);
-    ProjectOut_(ECon_[2], ECon_[0], GCov);
-    ProjectOut_(ECon_[2], ECon_[1], GCov);
-    Normalize_(ECon_[2], GCov);
-    ProjectOut_(ECon_[3], ECon_[0], GCov);
-    ProjectOut_(ECon_[3], ECon_[1], GCov);
-    ProjectOut_(ECon_[3], ECon_[2], GCov);
-    Normalize_(ECon_[3], GCov);
+    Normalize_(Econ_[0], Gcov);
+    ProjectOut_(Econ_[1], Econ_[0], Gcov);
+    Normalize_(Econ_[1], Gcov);
+    ProjectOut_(Econ_[2], Econ_[0], Gcov);
+    ProjectOut_(Econ_[2], Econ_[1], Gcov);
+    Normalize_(Econ_[2], Gcov);
+    ProjectOut_(Econ_[3], Econ_[0], Gcov);
+    ProjectOut_(Econ_[3], Econ_[1], Gcov);
+    ProjectOut_(Econ_[3], Econ_[2], Gcov);
+    Normalize_(Econ_[3], Gcov);
 
     // Make covariant version
-    MULOOP { Lower_(ECon_[mu], GCov, ECov_[mu]); }
-    MULOOP { ECov_[0][mu] *= -1.; }
+    SPACETIMELOOP(mu) { Lower_(Econ_[mu], Gcov, Ecov_[mu]); }
+    SPACETIMELOOP(mu) { Ecov_[0][mu] *= -1.; }
   }
 
   KOKKOS_INLINE_FUNCTION
   void CoordToTetradCon(const Real VCoord[NDFULL], Real VTetrad[NDFULL]) {
-    MULOOP {
+    SPACETIMELOOP(mu) {
       VTetrad[mu] = 0.;
-      NULOOP { VTetrad[mu] += ECov_[mu][nu] * VCoord[nu]; }
+      SPACETIMELOOP(nu) { VTetrad[mu] += Ecov_[mu][nu] * VCoord[nu]; }
     }
   }
 
   KOKKOS_INLINE_FUNCTION
   void CoordToTetradCov(const Real VCoord[NDFULL], Real VTetrad[NDFULL]) {
-    MULOOP {
+    SPACETIMELOOP(mu) {
       VTetrad[mu] = 0.;
-      NULOOP { VTetrad[mu] += ECon_[mu][nu] * VCoord[nu]; }
+      SPACETIMELOOP(nu) { VTetrad[mu] += Econ_[mu][nu] * VCoord[nu]; }
     }
   }
 
   KOKKOS_INLINE_FUNCTION
   void TetradToCoordCon(const Real VTetrad[NDFULL], Real VCoord[NDFULL]) {
-    MULOOP {
+    SPACETIMELOOP(mu) {
       VCoord[mu] = 0.;
-      NULOOP { VCoord[mu] += ECon_[nu][mu] * VTetrad[nu]; }
+      SPACETIMELOOP(nu) { VCoord[mu] += Econ_[nu][mu] * VTetrad[nu]; }
     }
   }
 
   KOKKOS_INLINE_FUNCTION
   void TetradToCoordCov(const Real VTetrad[NDFULL], Real VCoord[NDFULL]) {
-    MULOOP {
+    SPACETIMELOOP(mu) {
       VCoord[mu] = 0.;
-      NULOOP { VCoord[mu] += ECov_[nu][mu] * VTetrad[nu]; }
+      SPACETIMELOOP(nu) { VCoord[mu] += Ecov_[nu][mu] * VTetrad[nu]; }
     }
   }
 
 private:
   KOKKOS_INLINE_FUNCTION
-  void Normalize_(double VCon[NDFULL], const double GCov[NDFULL][NDFULL]) {
+  void Normalize_(double Vcon[NDFULL], const double Gcov[NDFULL][NDFULL]) {
     double norm = 0.;
-    MULOOP NULOOP { norm += VCon[mu] * VCon[nu] * GCov[mu][nu]; }
+    SPACETIMELOOP(mu) SPACETIMELOOP(nu) { norm += Vcon[mu] * Vcon[nu] * Gcov[mu][nu]; }
 
     norm = sqrt(fabs(norm));
-    MULOOP { VCon[mu] /= norm; }
+    SPACETIMELOOP(mu) { Vcon[mu] /= norm; }
   }
 
   KOKKOS_INLINE_FUNCTION
-  void ProjectOut_(double VCona[NDFULL], double VConb[NDFULL],
-                   const double GCov[NDFULL][NDFULL]) {
-    double VConb_sq = 0.;
-    MULOOP NULOOP { VConb_sq += VConb[mu] * VConb[nu] * GCov[mu][nu]; }
+  void ProjectOut_(double Vcona[NDFULL], double Vconb[NDFULL],
+                   const double Gcov[NDFULL][NDFULL]) {
+    double Vconb_sq = 0.;
+    SPACETIMELOOP(mu) SPACETIMELOOP(nu) { Vconb_sq += Vconb[mu] * Vconb[nu] * Gcov[mu][nu]; }
 
     double adotb = 0.;
-    MULOOP NULOOP { adotb += VCona[mu] * VConb[nu] * GCov[mu][nu]; }
+    SPACETIMELOOP(mu) SPACETIMELOOP(nu) { adotb += Vcona[mu] * Vconb[nu] * Gcov[mu][nu]; }
 
-    MULOOP { VCona[mu] -= VConb[mu] * adotb / VConb_sq; }
+    SPACETIMELOOP(mu) { Vcona[mu] -= Vconb[mu] * adotb / Vconb_sq; }
   }
 
   KOKKOS_INLINE_FUNCTION
-  void Lower_(const double VCon[NDFULL], const double GCov[NDFULL][NDFULL],
-              double VCov[NDFULL]) {
-    VCov[0] = GCov[0][0] * VCon[0] + GCov[0][1] * VCon[1] + GCov[0][2] * VCon[2] +
-              GCov[0][3] * VCon[3];
-    VCov[1] = GCov[1][0] * VCon[0] + GCov[1][1] * VCon[1] + GCov[1][2] * VCon[2] +
-              GCov[1][3] * VCon[3];
-    VCov[2] = GCov[2][0] * VCon[0] + GCov[2][1] * VCon[1] + GCov[2][2] * VCon[2] +
-              GCov[2][3] * VCon[3];
-    VCov[3] = GCov[3][0] * VCon[0] + GCov[3][1] * VCon[1] + GCov[3][2] * VCon[2] +
-              GCov[3][3] * VCon[3];
+  void Lower_(const double Vcon[NDFULL], const double Gcov[NDFULL][NDFULL],
+              double Vcov[NDFULL]) {
+    Vcov[0] = Gcov[0][0] * Vcon[0] + Gcov[0][1] * Vcon[1] + Gcov[0][2] * Vcon[2] +
+              Gcov[0][3] * Vcon[3];
+    Vcov[1] = Gcov[1][0] * Vcon[0] + Gcov[1][1] * Vcon[1] + Gcov[1][2] * Vcon[2] +
+              Gcov[1][3] * Vcon[3];
+    Vcov[2] = Gcov[2][0] * Vcon[0] + Gcov[2][1] * Vcon[1] + Gcov[2][2] * Vcon[2] +
+              Gcov[2][3] * Vcon[3];
+    Vcov[3] = Gcov[3][0] * Vcon[0] + Gcov[3][1] * Vcon[1] + Gcov[3][2] * Vcon[2] +
+              Gcov[3][3] * Vcon[3];
   }
 
   KOKKOS_INLINE_FUNCTION
-  void Raise_(const double VCov[NDFULL], const double GCon[NDFULL][NDFULL],
-              double VCon[NDFULL]) {
-    VCon[0] = GCon[0][0] * VCov[0] + GCon[0][1] * VCov[1] + GCon[0][2] * VCov[2] +
-              GCon[0][3] * VCov[3];
-    VCon[1] = GCon[1][0] * VCov[0] + GCon[1][1] * VCov[1] + GCon[1][2] * VCov[2] +
-              GCon[1][3] * VCov[3];
-    VCon[2] = GCon[2][0] * VCov[0] + GCon[2][1] * VCov[1] + GCon[2][2] * VCov[2] +
-              GCon[2][3] * VCov[3];
-    VCon[3] = GCon[3][0] * VCov[0] + GCon[3][1] * VCov[1] + GCon[3][2] * VCov[2] +
-              GCon[3][3] * VCov[3];
+  void Raise_(const double Vcov[NDFULL], const double Gcon[NDFULL][NDFULL],
+              double Vcon[NDFULL]) {
+    Vcon[0] = Gcon[0][0] * Vcov[0] + Gcon[0][1] * Vcov[1] + Gcon[0][2] * Vcov[2] +
+              Gcon[0][3] * Vcov[3];
+    Vcon[1] = Gcon[1][0] * Vcov[0] + Gcon[1][1] * Vcov[1] + Gcon[1][2] * Vcov[2] +
+              Gcon[1][3] * Vcov[3];
+    Vcon[2] = Gcon[2][0] * Vcov[0] + Gcon[2][1] * Vcov[1] + Gcon[2][2] * Vcov[2] +
+              Gcon[2][3] * Vcov[3];
+    Vcon[3] = Gcon[3][0] * Vcov[0] + Gcon[3][1] * Vcov[1] + Gcon[3][2] * Vcov[2] +
+              Gcon[3][3] * Vcov[3];
   }
 
   KOKKOS_INLINE_FUNCTION
@@ -161,12 +163,13 @@ private:
     }
   }
 
-  Real ECon_[NDFULL][NDFULL];
-  Real ECov_[NDFULL][NDFULL];
+  Real Econ_[NDFULL][NDFULL];
+  Real Ecov_[NDFULL][NDFULL];
 };
 
-#undef MULOOP
-#undef NULOOP
+#undef SPACETIMELOOP
+//#undef MULOOP
+//#undef SPACETIMELOOP(nu)
 
 } // namespace Geometry
 
