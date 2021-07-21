2682f6924d550be5d88c803fa337090d9ec37728
Ben R. Ryan <brryan@lanl.gov>
2021-07-21 15:02:33 -0600

    Get hydro working again

--- src/geometry/tetrads.hpp
+++ src/geometry/tetrads.hpp
@@ -1,9 +1,20 @@
-#include <parthenon/package.hpp>
-
-//#include "geometry/coordinate_systems.hpp"
+// © 2021. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract
+// 89233218CNA000001 for Los Alamos National Laboratory (LANL), which
+// is operated by Triad National Security, LLC for the U.S.
+// Department of Energy/National Nuclear Security Administration. All
+// rights in the program are reserved by Triad National Security, LLC,
+// and the U.S. Department of Energy/National Nuclear Security
+// Administration. The Government is granted for itself and others
+// acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+// license in this material to reproduce, prepare derivative works,
+// distribute copies to the public, perform publicly and display
+// publicly, and to permit others to do so.
 
 #ifndef GEOMETRY_TETRADS_HPP_
 #define GEOMETRY_TETRADS_HPP_
+
+#include <parthenon/package.hpp>
 
 namespace Geometry {
 
