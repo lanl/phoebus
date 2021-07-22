8bc74e2498a0c7f8f21e0e48cd8a1add88e85a85
Ben R. Ryan <brryan@lanl.gov>
2021-07-22 12:59:15 -0600

    Add neutrino species selector to cooling function

--- src/radiation/radiation.cpp
+++ src/radiation/radiation.cpp
@@ -1,17 +1,15 @@
-//========================================================================================
-// (C) (or copyright) 2021. Triad National Security, LLC. All rights reserved.
-//
-// This program was produced under U.S. Government contract 89233218CNA000001
-// for Los Alamos National Laboratory (LANL), which is operated by Triad
-// National Security, LLC for the U.S. Department of Energy/National Nuclear
-// Security Administration. All rights in the program are reserved by Triad
-// National Security, LLC, and the U.S. Department of Energy/National Nuclear
-// Security Administration. The Government is granted for itself and others
-// acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license
-// in this material to reproduce, prepare derivative works, distribute copies to
-// the public, perform publicly and display publicly, and to permit others to do
-// so.
-//========================================================================================
+// © 2021. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract
+// 89233218CNA000001 for Los Alamos National Laboratory (LANL), which
+// is operated by Triad National Security, LLC for the U.S.
+// Department of Energy/National Nuclear Security Administration. All
+// rights in the program are reserved by Triad National Security, LLC,
+// and the U.S. Department of Energy/National Nuclear Security
+// Administration. The Government is granted for itself and others
+// acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+// license in this material to reproduce, prepare derivative works,
+// distribute copies to the public, perform publicly and display
+// publicly, and to permit others to do so.
 
 #include "radiation.hpp"
 #include "geometry/geometry.hpp"
@@ -53,6 +51,15 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
     PARTHENON_FAIL(msg);
   }
 
+  // Set which neutrino species to include in simulation
+  bool do_nu_electron = pin->GetOrAddBoolean("radiation", "do_nu_electron", true);
+  params.Add("do_nu_electron", do_nu_electron);
+  bool do_nu_electron_anti = pin->GetOrAddBoolean("radiation", "do_nu_electron_anti", true);
+  params.Add("do_nu_electron_anti", do_nu_electron_anti);
+  bool do_nu_heavy = pin->GetOrAddBoolean("radiation", "do_nu_heavy", true);
+  params.Add("do_nu_heavy", do_nu_heavy);
+
+  // Initialize frequency discretization
   Real nu_min = pin->GetReal("radiation", "nu_min");
   params.Add("nu_min", nu_min);
   Real nu_max = pin->GetReal("radiation", "nu_max");
--- src/radiation/radiation.hpp
+++ src/radiation/radiation.hpp
@@ -1,17 +1,15 @@
-//========================================================================================
-// (C) (or copyright) 2021. Triad National Security, LLC. All rights reserved.
-//
-// This program was produced under U.S. Government contract 89233218CNA000001
-// for Los Alamos National Laboratory (LANL), which is operated by Triad
-// National Security, LLC for the U.S. Department of Energy/National Nuclear
-// Security Administration. All rights in the program are reserved by Triad
-// National Security, LLC, and the U.S. Department of Energy/National Nuclear
-// Security Administration. The Government is granted for itself and others
-// acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license
-// in this material to reproduce, prepare derivative works, distribute copies to
-// the public, perform publicly and display publicly, and to permit others to do
-// so.
-//========================================================================================
+// © 2021. Triad National Security, LLC. All rights reserved.  This
+// program was produced under U.S. Government contract
+// 89233218CNA000001 for Los Alamos National Laboratory (LANL), which
+// is operated by Triad National Security, LLC for the U.S.
+// Department of Energy/National Nuclear Security Administration. All
+// rights in the program are reserved by Triad National Security, LLC,
+// and the U.S. Department of Energy/National Nuclear Security
+// Administration. The Government is granted for itself and others
+// acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+// license in this material to reproduce, prepare derivative works,
+// distribute copies to the public, perform publicly and display
+// publicly, and to permit others to do so.
 
 #ifndef RADIATION_HPP_
 #define RADIATION_HPP_
@@ -21,6 +19,8 @@
 #include <parthenon/package.hpp>
 #include <utils/error_checking.hpp>
 using namespace parthenon::package::prelude;
+
+#include <singularity-opac/neutrinos/opac_neutrinos.hpp>
 
 #include "compile_constants.hpp"
 #include "phoebus_utils/unit_conversions.hpp"
@@ -38,6 +38,9 @@ using namespace parthenon;
 namespace radiation {
 
 using pc = parthenon::constants::PhysicalConstants<parthenon::constants::CGS>;
+using singularity::RadiationType;
+
+constexpr RadiationType species[3] = {RadiationType::NU_ELECTRON, RadiationType::NU_ELECTRON_ANTI, RadiationType::NU_HEAVY};
 
 // TODO(BRR) Utilities that should be moved
 #define SMALL (1.e-200)
