25503b7fb6594c0277ea6205b92d6d01a86ee76b
Ben R. Ryan <brryan@lanl.gov>
2021-08-03 09:54:52 -0600

    Added unit test

--- tst/test_utils.hpp
+++ tst/test_utils.hpp
@@ -14,6 +14,16 @@
 #ifndef TEST_UTILS_HPP_
 #define TEST_UTILS_HPP_
 
+/*
+ * Test for near-machine-precision equivalence of Real values
+ *
+ * PARAM[IN] - val - Value to test equivalence of
+ * PARAM[IN] - ref - Value against which to test equivalence
+ * PARAM[IN] - tol - Tolerance of equivalent test
+ * PARAM[IN] - ignore_small - If both val and ref are small compared to tol, return true
+ *
+ * RETURN - True if val and
+ */
 KOKKOS_INLINE_FUNCTION
 bool SoftEquiv(const Real &val, const Real &ref, const Real tol = 1.e-8,
                const bool ignore_small = true) {
@@ -28,6 +38,36 @@ bool SoftEquiv(const Real &val, const Real &ref, const Real tol = 1.e-8,
   } else {
     return false;
   }
+}
+
+/* Create a dummy MeshBlock with a single dummy package and field. 1D grid assumed. Useful for
+ * constructing other objects e.g. MeshBlockData<Real> and CoordSysMeshBlock
+ *
+ * PARAM[IN] - nzones - Number of zones in x direction
+ *
+ * RETURN shared_ptr to trivial MeshBlock
+ */
+std::shared_ptr<MeshBlock> inline GetDummyMeshBlock(const int nzones = 8) {
+  return std::make_shared<MeshBlock>(nzones, 1);
+}
+
+/*
+ * Create a dummy MeshBlockData with a single dummy package and field. 1D grid assumed. Useful for
+ * constructing other objects e.g. CoordSysMeshBlocks.
+ *
+ * PARAM[IN] - pmb - shared_ptr to existing MeshBlock
+ *
+ * RETURN Trivial MeshBlockData<Real>
+ */
+std::shared_ptr<MeshBlockData<Real>> inline GetDummyMeshBlockData(std::shared_ptr<MeshBlock> pmb) {
+  auto pkg = std::make_shared<StateDescriptor>("Dummy package");
+  std::vector<int> scalar_shape{1, 1, 1};
+  Metadata m({Metadata::Independent, Metadata::WithFluxes}, scalar_shape);
+  pkg->AddField("dummy_field", m);
+
+  auto rc = std::make_shared<MeshBlockData<Real>>();
+  rc.get()->Initialize(pkg, pmb);
+  return rc;
 }
 
 #endif // TEST_UTILS_HPP_
