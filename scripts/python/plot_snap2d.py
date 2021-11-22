f6df16fd76ecdec419fc7792984d54ccb6369dca
Jonah Miller <jonahm@lanl.gov>
2021-11-22 15:14:32 -0700

    2d snapshot script

---
+++ scripts/python/plot_snap2d.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# =========================================================================================
+# (C) (or copyright) 2021. Triad National Security, LLC. All rights reserved.
+#
+# This program was produced under U.S. Government contract 89233218CNA000001 for Los
+# Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
+# for the U.S. Department of Energy/National Nuclear Security Administration. All rights
+# in the program are reserved by Triad National Security, LLC, and the U.S. Department
+# of Energy/National Nuclear Security Administration. The Government is granted for
+# itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+# license in this material to reproduce, prepare derivative works, distribute copies to
+# the public, perform publicly and display publicly, and to permit others to do so.
+# =========================================================================================
+
+"""This is a python script for plotting 2d simulations (but only 2d for now)
+that leverages the coords output so it always gets the geometry carrect and plots in
+x,y coordinates.
+"""
+
+from __future__ import print_function
+from argparse import ArgumentParser
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+
+# Assumes phdf in global python path
+try:
+    from parthenon_tools.phdf import phdf
+except ModuleNotFoundError:
+    from phdf import phdf
+
+def plot_dump(filename, varname,
+              savename,
+              plane='xz',
+              cbar_label = None,
+              x1bounds=None, x2bounds=None,
+              cbarbounds=None,
+              colormap='viridis',
+              log=True):
+    data = phdf(filename)
+    print(data)
+
+    q = data.Get(varname, False)
+    NB = q.shape[0]
+
+    if log:
+        q = np.log10(np.abs(q))
+
+    # We have to play this stupid game because
+    # outputs for variables of non-standard shape don't work
+    coordname = "g.c.coord"
+    coord = data.Get(coordname, False)
+    z = coord[...,3]
+    y = coord[...,2]
+    x = coord[...,1]
+
+    if plane == 'xz':
+        rho = np.sqrt(x**2 + y**2)
+        x = rho
+        y = z
+
+    if cbarbounds is not None:
+        qmin,qmax = cbarbounds
+    else:
+        qmin = q.min()
+        qmax = q.max()
+
+    # Aligned to cell centers if NONSTANDARD_SHAPE_IO_BROKEN
+    # but whatever
+    if x1bounds is None:
+        x1bounds = [x.min(), x.max()]
+    if x2bounds is None:
+        x2bounds = [y.min(), y.max()]
+
+    if cbar_label is None:
+        cbar_label = varname
+        if log:
+            cbar_label = r'$\log_{10}$' + cbar_label
+
+    fig = plt.figure()
+    p = fig.add_subplot(111, aspect=1)
+    for i in range(NB):
+        mesh = p.pcolormesh(x[i,0,:,:], y[i,0,:,:], q[i,0,:,:], shading='gouraud',
+                            vmin=qmin, vmax=qmax, cmap=colormap)
+
+    plt.xlim(x1bounds[0], x1bounds[1])
+    plt.ylim(x2bounds[0], x2bounds[1])
+
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$y$')
+    plt.colorbar(mesh, label=cbar_label)
+
+    plt.savefig(savename, dpi=300, bbox_inches='tight')
+    plt.close()
+    return
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Plot a 2d simulation snapshot.")
+    parser.add_argument('--plane', type=str, default='xz',
+                        choices=['xy','xz'],
+                        help='plane to project onto')
+    parser.add_argument('--xbounds', type=float, nargs=2, default=None,
+                        help='min and max bounds for x')
+    parser.add_argument('--ybounds', type=float, nargs=2, default=None,
+                        help='min and max bounds for y')
+    parser.add_argument('--cbarbounds', type=float, nargs=2, default=None,
+                        help='min and max values of colorbar')
+    parser.add_argument('--cmap', type=str, default='viridis',
+                        help='colormap')
+    parser.add_argument('--cbarlabel', type=str, default=None,
+                        help='Color bar label')
+    parser.add_argument('-s','--saveprefix', type=str,
+                        default="", help='Prefix for file save names')
+    parser.add_argument('-l','--linear',action='store_true',
+                        help='Use linear, instead of log scale')
+    parser.add_argument('--pdf', action='store_true',
+                        help="Save as pdf instead of png")
+    parser.add_argument('varname', type=str, help='Variable to plot')
+    parser.add_argument('files', type=str, nargs='+',
+                        help='Files to take a snapshot of')
+    args = parser.parse_args()
+    for i,f in enumerate(args.files):
+        log = not args.linear
+        postfix = '.pdf' if args.pdf else '.png'
+        savename = args.saveprefix + str(i).rjust(5,"0") + postfix
+        print(savename)
+        plot_dump(f, args.varname, savename,
+                  plane = args.plane,
+                  cbar_label = args.cbarlabel,
+                  x1bounds = args.xbounds,
+                  x2bounds = args.ybounds,
+                  cbarbounds = args.cbarbounds,
+                  colormap = args.cmap,
+                  log = log)
+
