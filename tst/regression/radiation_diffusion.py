786e498ab722feb4d08b805640c95e955d27d7f3
Luke Roberts <lfroberts@lanl.gov>
2022-02-18 11:21:23 -0700

    Update radiation test problems to work with regression refactor

--- tst/regression/radiation_diffusion.py
+++ tst/regression/radiation_diffusion.py
@@ -14,13 +14,16 @@
 # publicly, and to permit others to do so.
 
 import argparse
+import os 
+import sys 
 import regression_test as rt
 
 parser = argparse.ArgumentParser(description='Run a finite velocity radiation diffusion test')
 parser.add_argument('--upgold', dest='upgold', action='store_true')
 parser.add_argument('--use_gpu', dest='use_gpu', action='store_true')
-parser.add_argument('--save_output', dest='save_output', action='store_true')
+parser.add_argument('--input', type=str, default=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../inputs/radiation_advection.pin'))
 parser.add_argument('--executable', type=str, default=None)
+parser.add_argument('--build_type', type=str, default='Release', choices=['Debug, Release'])
 args = parser.parse_args()
 
 modified_inputs = {}
@@ -35,19 +38,14 @@ modified_inputs['radiation_advection/vx'] = 0.3
 modified_inputs['radiation_advection/width'] = 0.0333
 modified_inputs['radiation_advection/kappas_init'] = 1.e3
 
-if args.executable == None:
-  rt.build_code(geometry="Minkowski", use_gpu=args.use_gpu)
-  rt.gold_comparison(variables=['r.p.J', 'r.p.H'],
-                     input_file='../../../inputs/radiation_advection.pin',
-                     modified_inputs=modified_inputs,
-                     upgold=args.upgold,
-                     compression_factor=10,
-                     save_output=args.save_output)
-else:
-  rt.gold_comparison(variables=['r.p.J', 'r.p.H'],
-                     input_file='../../../inputs/radiation_advection.pin',
-                     modified_inputs=modified_inputs,
-                     executable=args.executable,
-                     upgold=args.upgold,
-                     compression_factor=10,
-                     save_output=args.save_output)
+code = rt.gold_comparison(variables=['r.p.J', 'r.p.H'],
+                          input_file=args.input,
+                          modified_inputs=modified_inputs,
+                          executable=args.executable,
+                          geometry='Minkowski',
+                          use_gpu=args.use_gpu,
+                          build_type=args.build_type, 
+                          upgold=args.upgold,
+                          compression_factor=10)
+
+sys.exit(code)
