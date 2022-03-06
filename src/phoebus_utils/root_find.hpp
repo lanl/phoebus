f31f0f9c8f857d199df8ba5ed58932d4635ea211
Josh Dolence <jdolence@lanl.gov>
2022-03-06 13:58:23 -0700

    playing with things

--- src/phoebus_utils/root_find.hpp
+++ src/phoebus_utils/root_find.hpp
@@ -115,9 +115,11 @@ struct RootFind {
       Real c = (a*yb - b*ya)/(yb - ya);
       // guard against roundoff because ya or yb is sufficiently close to zero
       if (c == a) {
+        //printf("abort1: %g %g %g  %g %g\n", a, c, b, ya, yb);
         b = a;
         continue;
       } else if (c == b) {
+        //printf("abort2: %g %g %g  %g %g\n", a, c, b, ya, yb);
         a = b;
         continue;
       }
@@ -127,12 +129,14 @@ struct RootFind {
         yb = yc;
         b1++;
         ya *= (b1 > 1 ? 0.5 : 1.0);
+        //b1 = (b1 > 1 ? 0 : b1);
         b2 = 0;
       } else if (yc < 0.0) {
         a = c;
         ya = yc;
         b2++;
         yb *= (b2 > 1 ? 0.5 : 1.0);
+        //b2 = (b2 > 1 ? 0 : b2);
         b1 = 0;
       } else {
         a = c;
@@ -168,14 +172,6 @@ struct RootFind {
     while (b-a > 2.0*tol && iteration_count < max_iter) {
       const Real xh = 0.5*(a + b);
       const Real xf = (yb*a - ya*b)/(yb - ya);
-      // guard against roundoff because ya or yb is sufficiently close to zero
-      if (xf == a) {
-        b = a;
-        continue;
-      } else if (xf == b) {
-        a = b;
-        continue;
-      }
       const Real xhxf = xh - xf;
       const Real delta = std::min(kappa1*std::pow(b-a,kappa2),std::abs(xhxf));
       const Real sigma = (xhxf > 0 ? 1.0 : -1.0);
