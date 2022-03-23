241c1e8365e9e9c0dd3127512c8e62d06e405c77
Benjamin Ransom Ryan <brryan@lanl.gov>
2022-03-23 15:03:38 -0600

    Working to solve my bvals bug

--- src/phoebus_driver.cpp
+++ src/phoebus_driver.cpp
@@ -87,6 +87,7 @@ TaskListStatus PhoebusDriver::Step() {
 }
 
 void PhoebusDriver::PostInitializationCommunication() {
+  return;
   TaskCollection tc;
   TaskID none(0);
   BlockList_t &blocks = pmesh->block_list;
@@ -105,7 +106,7 @@ void PhoebusDriver::PostInitializationCommunication() {
     auto pmb = blocks[ib].get();
     auto &tl = async_region[ib];
     auto &sc = pmb->meshblock_data.Get();
-    auto &md = pmesh->mesh_data.GetOrAdd(stage_name[0], ib);
+    auto &md = pmesh->mesh_data.GetOrAdd(stage_name[0], ib); // TODO(BRR) This gives an empty md
 
     auto start_recv = tl.AddTask(none, &MeshBlockData<Real>::StartReceiving, sc.get(),
                                  BoundaryCommSubset::all);
