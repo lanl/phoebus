dbdc5b3a48c47d0c7cd3e7e7d0c846e569783d12
Jonah Maxwell Miller <jonahm@lanl.gov>
2022-04-28 20:07:36 -0600

    clean up cmake as it has unneeded complexity. Thanks Ben.

--- cmake/Geometry.cmake
+++ cmake/Geometry.cmake
@@ -31,28 +31,11 @@ set(PHOEBUS_ANALYTIC_GEOMETRIES
     "MonopoleCart"
     )
 
-# List geometries that should not be cached here
-set(PHOEBUS_GEOMETRY_NO_CACHE
-  # "MonopoleSph"
-  )
-
-# List geometries that MUST be cached here
-set(PHOEBUS_GEOMETRY_MUST_CACHE
-  # "MonopoleCart"
-  )
-
 if(PHOEBUS_ANALYTIC_GEOMETRY)
   if (PHOEBUS_GEOMETRY IN_LIST PHOEBUS_ANALYTIC_GEOMETRIES)
     set(GEOMETRY_MESH "Analytic<${PHOEBUS_GEOMETRY}, IndexerMesh>")
     set(GEOMETRY_MESH_BLOCK "Analytic<${PHOEBUS_GEOMETRY}, IndexerMeshBlock>")
-    if (PHOEBUS_GEOMETRY IN_LIST PHOEBUS_GEOMETRY_MUST_CACHE)
-      message(WARNING "The selected geometry must be cached. Enabling cache.")
-      set(PHOEBUS_CACHE_GEOMETRY ON CACHE BOOL "Force geometry to be cached" FORCE)
-    endif()
     if (PHOEBUS_CACHE_GEOMETRY)
-      if (PHOEBUS_GEOMETRY IN_LIST PHOEBUS_GEOMETRY_NO_CACHE)
-	message(FATAL_ERROR "The selected geometry does not support caching.")
-      endif()
       set(GEOMETRY_MESH "CachedOverMesh<${GEOMETRY_MESH}>")
       set(GEOMETRY_MESH_BLOCK "CachedOverMeshBlock<${GEOMETRY_MESH_BLOCK}>")
     endif()
