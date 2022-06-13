0c1762467e963a7d3f64fa1231bfbebc5bcd1a86
Ben Ryan <brryan@lanl.gov>
2022-06-13 10:14:51 -0600

    monte carlo cooling test no longer working

--- src/radiation/monte_carlo.cpp
+++ src/radiation/monte_carlo.cpp
@@ -312,7 +312,7 @@ TaskStatus MonteCarloSourceParticles(MeshBlock *pmb, MeshBlockData<Real> *rc,
 
               for (int mu = Gcov_lo; mu <= Gcov_lo + 3; mu++) {
                 // detG is in both numerator and denominator
-                v(mu, k, j, i) -= 1. / (d3x * dt) * weight(m) * K_coord[mu - Gcov_lo];
+                v(mu, k, j, i) += 1. / (d3x * dt) * weight(m) * K_coord[mu - Gcov_lo];
               }
               // TODO(BRR) lepton sign
               v(Gye, k, j, i) -= 1. / (d3x * dt) * Ucon[0] * weight(m) * mp_code;
