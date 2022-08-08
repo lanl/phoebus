9869e8343d7ca6b890237f9641ee3ae17ac1cf75
Jonah Maxwell Miller <jonahm@lanl.gov>
2022-08-08 12:14:28 -0600

    addres Luke comments

--- scripts/bash/get-zs-p-ws.sh
+++ scripts/bash/get-zs-p-ws.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Used to pull out the zone-cycles/wallsecond from a phoebus or parthenon
+# output file. Can be combined with other tools for bash scripting
+# usage
+# ZC=$(get-zs-p-ws.sh torus.out)
+# echo ${ZC}
+
 echo $(grep 'zone-cycles/wallsecond = ' ${1} | cut -d '=' -f 2 | xargs)
