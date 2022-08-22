564f04e12d279fe6ce0a6f0981c502e503262f2d
Benjamin Ransom Ryan <brryan@lanl.gov>
2022-08-22 11:37:27 -0600

    No explosions when turning off reconstructed xi clamp

--- src/pgen/torus.cpp
+++ src/pgen/torus.cpp
@@ -90,11 +90,11 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
   bool do_rad = pmb->packages.Get("radiation")->Param<bool>("active");
 
   PackIndexMap imap;
-  auto v =
-      rc->PackVariables({fluid_prim::density, fluid_prim::velocity, fluid_prim::energy,
-                         fluid_prim::bfield, fluid_prim::ye, fluid_prim::pressure,
-                         fluid_prim::temperature, radmoment_prim::J, radmoment_prim::H},
-                        imap);
+  auto v = rc->PackVariables({fluid_prim::density, fluid_prim::velocity,
+                              fluid_prim::energy, fluid_prim::bfield, fluid_prim::ye,
+                              fluid_prim::pressure, fluid_prim::temperature,
+                              fluid_prim::gamma1, radmoment_prim::J, radmoment_prim::H},
+                             imap);
 
   const int irho = imap[fluid_prim::density].first;
   const int ivlo = imap[fluid_prim::velocity].first;
@@ -105,6 +105,7 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
   const int iye = imap[fluid_prim::ye].second;
   const int iprs = imap[fluid_prim::pressure].first;
   const int itmp = imap[fluid_prim::temperature].first;
+  const int igm1 = imap[fluid_prim::gamma1].first;
 
   vpack_types::FlatIdx iJ({-1}, -1);
   vpack_types::FlatIdx iH({-1}, -1);
@@ -262,6 +263,9 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
             v(irho, k, j, i), v(ieng, k, j, i) / v(irho, k, j, i), lambda);
         v(iprs, k, j, i) = eos.PressureFromDensityTemperature(v(irho, k, j, i),
                                                               v(itmp, k, j, i), lambda);
+        v(igm1, k, j, i) = eos.BulkModulusFromDensityTemperature(
+                               v(irho, k, j, i), v(itmp, k, j, i), lambda) /
+                           v(iprs, k, j, i);
 
         // Radiation
         if (do_rad) {
@@ -305,8 +309,8 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
               v(iH(ispec, 2), k, j, i) = 0.0;
               if (i == 64 && j == 64 && ispec == 0) {
                 printf("J: %e H: %e %e %e\n", v(iJ(ispec), k, j, i),
-                  v(iH(ispec, 0), k, j, i), v(iH(ispec, 1), k, j, i),
-                  v(iH(ispec, 2), k, j, i));
+                       v(iH(ispec, 0), k, j, i), v(iH(ispec, 1), k, j, i),
+                       v(iH(ispec, 2), k, j, i));
               }
             }
           } else {
