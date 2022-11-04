d18a6c8340e54b64ebbefea6704bed24e9be9248
Brandon Barker <bbarker.py@protonmail.ch>
2022-11-04 18:37:10 -0400

    feat: draft of AdvectTracers

--- src/tracers/tracers.cpp
+++ src/tracers/tracers.cpp
@@ -1,5 +1,11 @@
 // preamble.
 
+#include "phoebus_utils/phoebus_interpolation.hpp"
+#include "geometry/geometry.hpp"
+#include "geometry/geometry_utils.hpp"
+#include "phoebus_utils/cell_locations.hpp"
+#include "phoebus_utils/variables.hpp"
+#include "phoebus_utils/relativity_utils.hpp"
 #include "tracers.hpp"
 
 typedef Kokkos::Random_XorShift64_Pool<> RNGPool;
@@ -32,6 +38,7 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
 } // Initialize
 
 TaskStatus AdvectTracers(MeshBlockData<Real> *rc) {
+  using namespace LCInterp;
   auto pmb = rc->GetParentPointer();
   auto swarm = pmb->swarm_data.Get()->Get("tracers");
 
@@ -42,7 +49,47 @@ TaskStatus AdvectTracers(MeshBlockData<Real> *rc) {
   auto &z = swarm->Get<Real>("z").Get();
   
   auto swarm_d = swarm->GetDeviceContext();
+  namespace p = fluid_prim;
+
+  const std::vector<std::string> vars = {p::velocity};
+
+  PackIndexMap imap;
+  auto pack = rc->PackVariables(vars, imap);
+
+  const int pvel_lo = imap[p::velocity].first;
+  const int pvel_hi = imap[p::velocity].second;
+
+  auto geom = Geometry::GetCoordinateSystem(rc);
+
   // update loop.
+  pmb->par_for( 
+      "Advect Tracers", 0, max_active_index, KOKKOS_LAMBDA( const int n ) {
+        if (swarm_d.IsActive(n)) {
+          int k, j, i;
+          swarm_d.Xtoijk(x(n), y(n), z(n), i, j, k);
+
+          // geom quantities
+          Real gcov4[4][4];
+          geom.SpacetimeMetric(CellLocation::Cent, k, j, i, gcov4);
+          Real gcon[3][3];
+          geom.MetricInverse(CellLocation::Cent, k, j, i, gcon);
+          Real lapse = geom.Lapse(CellLocation::Cent, k, j, i);
+          Real shift[3];
+          geom.ContravariantShift(CellLocation::Cent, k, j, i, shift);
+          const Real vel[] = {pack(pvel_lo, k, j, i), pack(pvel_lo + 1, k, j, i),
+                              pack(pvel_hi, k, j, i)};
+          const Real W = phoebus::GetLorentzFactor(vel, gcov4);
+
+          // Get shift, W, lapse
+          Real vel_X1 = LCInterp::Do(0, x(n), y(n), z(n), pack, pvel_lo) / W; 
+          Real vel_X2 = LCInterp::Do(0, x(n), y(n), z(n), pack, pvel_lo + 1) / W; 
+          Real vel_X3 = LCInterp::Do(0, x(n), y(n), z(n), pack, pvel_hi) / W; 
+
+          x(n) += lapse * vel_X1 - shift[0];
+          y(n) += lapse * vel_X2 - shift[1];
+          z(n) += lapse * vel_X3 - shift[2];
+       } 
+      });  
 
   return TaskStatus::complete;
 } // AdvectTracers
