36463bbdf50f51199c0174f5c8a261c6a80c26ad
Brandon Barker <bbarker.py@protonmail.ch>
2023-02-23 05:30:43 +0000

    feat: add test for adiabats. cmake FetchContent to get SFHo table, enable with -DPHOEBUS_ENABLE_DOWNLOADS

--- tst/unit/phoebus_utils/test_adiabats.cpp
+++ tst/unit/phoebus_utils/test_adiabats.cpp
@@ -11,9 +11,9 @@
 // distribute copies to the public, perform publicly and display
 // publicly, and to permit others to do so.
 
-
 // stdlib includes
 #include <cmath>
+#include <limits>
 
 // external includes
 #include "catch2/catch.hpp"
@@ -26,16 +26,62 @@
 #include <kokkos_abstraction.hpp>
 #include <parameter_input.hpp>
 
+#include <spiner/databox.hpp>
+
 // Test utils
 #include "test_utils.hpp"
 
 // Relativity utils
 #include "phoebus_utils/adiabats.hpp"
 
-using namespace phoebus;
+// using namespace phoebus;
 using singularity::StellarCollapse;
 
 TEST_CASE("ADIABATS", "[compute_adiabats]") {
-  StellarCollapse eos("~/scratch/sfho_table.h5"); // TODO: update
-  REQUIRE(true);
+  GIVEN("A stellar collapse table") {
+    std::string filename = "./unit/phoebus_utils/table_dir/"
+                           "Hempel_SFHoEOS_rho222_temp180_ye60_version_1.1_20120817.h5";
+    StellarCollapse eos(filename);
+
+    const int nsamps = 180;
+    Spiner::DataBox rho_d(Spiner::AllocationTarget::Device, nsamps);
+    Spiner::DataBox temp_d(Spiner::AllocationTarget::Device, nsamps);
+    const Real lrho_min = eos.lRhoMin();
+    const Real lrho_max = eos.lRhoMax();
+    const Real T_min = std::pow(10.0, eos.lTMin());
+    const Real T_max = std::pow(10.0, eos.lTMax());
+
+    THEN("We can compute adiabats") {
+      /**
+       * Sample rho. For this adiabat, we do not go to the table upper limit
+       * May need generalization in the future [TODO(BLB)]
+       **/
+      SampleRho(rho_d, lrho_min, lrho_max - 1.5, nsamps);
+
+      const Real Ye = 0.1;
+      const Real S0 = 20.0;
+      ComputeAdiabats(rho_d, temp_d, eos, Ye, S0, T_min, T_max, nsamps);
+
+      AND_THEN("We verify that the adiabats produce the original entropy") {
+        // Given the adiabats, we should be able to get back the original entropy S0
+        int n_wrong = 1;
+        Kokkos::parallel_reduce(
+            "get n_wrong (adiabats)", nsamps,
+            KOKKOS_LAMBDA(const int i, int &update) {
+              Real lambda[2];
+              lambda[0] = Ye;
+              const Real rho = std::pow(10.0, rho_d(i));
+              const Real S_i = eos.EntropyFromDensityTemperature(rho, temp_d(i), lambda);
+              if (!SoftEquiv(S_i, S0, 1.e-6, false)) {
+                update += 1;
+              }
+            },
+            n_wrong);
+
+        free(rho_d);
+        free(temp_d);
+        REQUIRE(n_wrong == 0);
+      }
+    }
+  }
 }
