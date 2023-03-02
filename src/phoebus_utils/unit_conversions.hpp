96ef8605d59487834dfefb5bb3b217e189c51d91
Brandon Barker <bbarker.py@protonmail.ch>
2023-03-02 00:55:56 +0000

    draft entropy unit conversion

--- src/phoebus_utils/unit_conversions.hpp
+++ src/phoebus_utils/unit_conversions.hpp
@@ -50,6 +50,9 @@ class UnitConversions {
   Real GetTemperatureCodeToCGS() const { return temperature_; }
   Real GetTemperatureCGSToCode() const { return 1. / temperature_; }
 
+  Real GetEntropyCodeToCGS() const { return energy_ / temperature_ / mass_; }
+  Real GetEntropyCGSToCode() const { return mass_ * temperature_ / energy_; }
+
  private:
   bool scale_free_;
   Real mass_;
