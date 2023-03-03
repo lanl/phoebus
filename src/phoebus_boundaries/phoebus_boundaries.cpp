a18c3083c32a1c5f1e82374523c35f8eb9bc8e58
Ben R. Ryan <brryan@lanl.gov>
2023-03-03 13:11:26 -0700

    Cleanup

--- src/phoebus_boundaries/phoebus_boundaries.cpp
+++ src/phoebus_boundaries/phoebus_boundaries.cpp
@@ -102,11 +102,10 @@ void GenericBC(std::shared_ptr<MeshBlockData<Real>> &rc, bool coarse) {
         const Real gratio = robust::ratio(detg, detg_ref);
 
         if (rescale) {
-        q(l, k, j, i) = sgn * gratio * q(l, kref, jref, iref);
-        } else 
-        {
-        q(l, k, j, i) = sgn * q(l, kref, jref, iref);
-          }
+          q(l, k, j, i) = sgn * gratio * q(l, kref, jref, iref);
+        } else {
+          q(l, k, j, i) = sgn * q(l, kref, jref, iref);
+        }
       });
 }
 
@@ -163,19 +162,18 @@ void PolarInnerX2(std::shared_ptr<MeshBlockData<Real>> &rc, bool coarse) {
   const auto idx_pvel = imap.GetFlatIdx(fluid_prim::velocity, false);
   const auto idx_pb = imap.GetFlatIdx(fluid_prim::bfield, false);
 
-    pmb->par_for_bndry(
-        "PolarInnerX2Prim", nb, domain, coarse,
-        KOKKOS_LAMBDA(const int &l, const int &k, const int &j, const int &i) {
-          const int jref = -j + 2*j0 - 1; 
-          if (l == idx_pvel(1)) {
-            q(l, k, j, i) = -q(l, k, jref, i);
-          } else if (l == idx_pb(1)) {
-            //q(l, k, j, i) = -geom.Lapse(CellLocation::Cent, k, jref, i) / geom.Lapse(CellLocation::Cent, k, j, i)*q(l, k, jref, i);
-            q(l, k, j, i) = -q(l, k, jref, i);
-          } else {
-            q(l, k, j, i) = q(l, k, jref, i);
-          }
-        });
+  pmb->par_for_bndry(
+      "PolarInnerX2Prim", nb, domain, coarse,
+      KOKKOS_LAMBDA(const int &l, const int &k, const int &j, const int &i) {
+        const int jref = -j + 2 * j0 - 1;
+        if (l == idx_pvel(1)) {
+          q(l, k, j, i) = -q(l, k, jref, i);
+        } else if (l == idx_pb(1)) {
+          q(l, k, j, i) = -q(l, k, jref, i);
+        } else {
+          q(l, k, j, i) = q(l, k, jref, i);
+        }
+      });
 }
 
 void PolarOuterX2(std::shared_ptr<MeshBlockData<Real>> &rc, bool coarse) {
@@ -197,19 +195,18 @@ void PolarOuterX2(std::shared_ptr<MeshBlockData<Real>> &rc, bool coarse) {
   const auto idx_pvel = imap.GetFlatIdx(fluid_prim::velocity, false);
   const auto idx_pb = imap.GetFlatIdx(fluid_prim::bfield, false);
 
-    pmb->par_for_bndry(
-        "PolarOuterX2Prim", nb, domain, coarse,
-        KOKKOS_LAMBDA(const int &l, const int &k, const int &j, const int &i) {
-          const int jref = -j + 2*(j0 + 1) - 1; 
-          if (l == idx_pvel(1)) {
-            q(l, k, j, i) = -q(l, k, jref, i);
-          } else if (l == idx_pb(1)) {
-            q(l, k, j, i) = -q(l, k, jref, i);
-            //q(l, k, j, i) = -geom.Lapse(CellLocation::Cent, k, jref, i) / geom.Lapse(CellLocation::Cent, k, j, i)*q(l, k, jref, i);
-          } else {
-            q(l, k, j, i) = q(l, k, jref, i);
-          }
-        });
+  pmb->par_for_bndry(
+      "PolarOuterX2Prim", nb, domain, coarse,
+      KOKKOS_LAMBDA(const int &l, const int &k, const int &j, const int &i) {
+        const int jref = -j + 2 * (j0 + 1) - 1;
+        if (l == idx_pvel(1)) {
+          q(l, k, j, i) = -q(l, k, jref, i);
+        } else if (l == idx_pb(1)) {
+          q(l, k, j, i) = -q(l, k, jref, i);
+        } else {
+          q(l, k, j, i) = q(l, k, jref, i);
+        }
+      });
 }
 
 void OutflowOuterX1(std::shared_ptr<MeshBlockData<Real>> &rc, bool coarse) {
@@ -310,99 +307,6 @@ TaskStatus ConvertBoundaryConditions(std::shared_ptr<MeshBlockData<Real>> &rc) {
   auto &pkg_rad = rc->GetParentPointer()->packages.Get("radiation");
   auto &pkg_fix = rc->GetParentPointer()->packages.Get("fixup");
   std::string bc_vars = pkg->Param<std::string>("bc_vars");
-  
-  // Apply inflow check to ix1 for BH problem at simulation BC only
-  //const bool enable_ix1_fmks_inflow_check = true;
-  //if (typeid(PHOEBUS_GEOMETRY) == typeid(Geometry::FMKS) &&
-  //    enable_ix1_fmks_inflow_check &&
-  //    pmb->boundary_flag[0] != parthenon::BoundaryFlag::block) {
-
-  //  const IndexDomain domain = IndexDomain::inner_x1;
-  //  IndexRange ib = rc->GetBoundsI(domain);
-  //  IndexRange jb = rc->GetBoundsJ(domain);
-  //  IndexRange kb = rc->GetBoundsK(domain);
-  //  ib.e += 4;
-
-  //  if (bc_vars == "conserved") {
-  //    // Just call C2P everywhere instead of 6 different kernels
-  //    fluid::ConservedToPrimitiveRegion(rc.get(), ib, jb, kb);
-  //  }
-
-  //  // Inflow check and then p2c
-  //  std::shared_ptr<MeshBlock> pmb = rc->GetBlockPointer();
-  //  auto geom = Geometry::GetCoordinateSystem(rc.get());
-
-  //  // TODO(BRR) Is this always true?
-  //  const bool coarse = false;
-
-  //  PackIndexMap imap;
-  //  std::vector<std::string> vars{fluid_prim::velocity, radmoment_prim::H};
-  //  auto q = rc->PackVariables(vars, imap, coarse);
-  //  auto nb1 = IndexRange{0, 0};
-
-  //  const int pv_lo = imap[fluid_prim::velocity].first;
-  //  auto idx_H = imap.GetFlatIdx(radmoment_prim::H, false);
-
-  //  const int num_species =
-  //      pkg_rad->Param<bool>("active") ? pkg_rad->Param<int>("num_species") : 0;
-  //  pmb->par_for_bndry(
-  //      "OutflowInnerX1PrimFixup", nb1, domain, coarse,
-  //      KOKKOS_LAMBDA(const int &dummy, const int &k, const int &j, const int &i) {
-  //        // Enforce u^1 >= 0
-  //        Real vcon[3] = {q(pv_lo, k, j, i), q(pv_lo + 1, k, j, i),
-  //                        q(pv_lo + 2, k, j, i)};
-  //        Real gammacov[3][3];
-  //        geom.Metric(CellLocation::Cent, k, j, i, gammacov);
-  //        Real W = phoebus::GetLorentzFactor(vcon, gammacov);
-  //        const Real alpha = geom.Lapse(CellLocation::Cent, k, j, i);
-  //        Real beta[3];
-  //        geom.ContravariantShift(CellLocation::Cent, k, j, i, beta);
-
-  //        Real ucon1 = vcon[0] - W * beta[0] / alpha;
-
-  //        if (ucon1 > 0) {
-  //          SPACELOOP(ii) { vcon[ii] /= W; }
-  //          vcon[0] = beta[0] / alpha;
-  //          Real vsq = 0.;
-  //          SPACELOOP2(ii, jj) { vsq += gammacov[ii][jj] * vcon[ii] * vcon[jj]; }
-  //          W = 1. / sqrt(1. - vsq);
-
-  //          SPACELOOP(ii) { q(pv_lo + ii, k, j, i) = W * vcon[ii]; }
-  //        }
-
-  //        // No flux of radiation into the simulation
-  //        if (idx_H.IsValid()) {
-  //          Real gammacon[3][3];
-  //          geom.MetricInverse(CellLocation::Cent, k, j, i, gammacon);
-  //          for (int ispec = 0; ispec < num_species; ispec++) {
-  //            Real Hcon[3] = {0};
-  //            SPACELOOP2(ii, jj) {
-  //              Hcon[ii] += gammacon[ii][jj] * q(idx_H(ispec, jj), k, j, i);
-  //            }
-  //            if (Hcon[0] < 0.) {
-  //              Hcon[0] = 0.;
-
-  //              // Check xi
-  //              Real xi = 0.;
-  //              SPACELOOP2(ii, jj) { xi += gammacov[ii][jj] * Hcon[ii] * Hcon[jj]; }
-  //              xi = std::sqrt(xi);
-  //              if (xi > 0.99) {
-  //                SPACELOOP(ii) { Hcon[ii] *= 0.99 / xi; }
-  //              }
-
-  //              SPACELOOP(ii) {
-  //                q(idx_H(ispec, ii), k, j, i) = 0.;
-  //                SPACELOOP(jj) {
-  //                  q(idx_H(ispec, ii), k, j, i) += gammacov[ii][jj] * Hcon[jj];
-  //                }
-  //              }
-  //            }
-  //          }
-  //        }
-  //      });
-
-  //  fluid::PrimitiveToConservedRegion(rc.get(), ib, jb, kb);
-  //}
 
   // Apply inflow check to ox1 for BH problem at simulation BC only
   const bool enable_ox1_fmks_inflow_check =
@@ -552,13 +456,17 @@ void ProcessBoundaryConditions(parthenon::ParthenonManager &pman) {
       pman.pinput->GetOrAddString("radiation", "method", "None");
 
   if (typeid(PHOEBUS_GEOMETRY) == typeid(Geometry::FMKS)) {
-    bool derefine_poles = pman.pinput->GetOrAddBoolean("coordinates", "derefine_poles", true);
+    bool derefine_poles =
+        pman.pinput->GetOrAddBoolean("coordinates", "derefine_poles", true);
     if (derefine_poles) {
-      const std::string ix2_bc = pman.pinput->GetOrAddString("phoebus", "ix2_bc", "outflow");
-      const std::string ox2_bc = pman.pinput->GetOrAddString("phoebus", "ox2_bc", "outflow");
-      PARTHENON_REQUIRE(ix2_bc != "reflect" && ix2_bc != "polar" &&
-        ox2_bc != "reflect" && ox2_bc != "polar",
-        "Polar and Reflecting X2 BCs not supported for \"derefine_poles = true\"!");
+      const std::string ix2_bc =
+          pman.pinput->GetOrAddString("phoebus", "ix2_bc", "outflow");
+      const std::string ox2_bc =
+          pman.pinput->GetOrAddString("phoebus", "ox2_bc", "outflow");
+      PARTHENON_REQUIRE(
+          ix2_bc != "reflect" && ix2_bc != "polar" && ox2_bc != "reflect" &&
+              ox2_bc != "polar",
+          "Polar and Reflecting X2 BCs not supported for \"derefine_poles = true\"!");
     }
   }
 
@@ -577,7 +485,7 @@ void ProcessBoundaryConditions(parthenon::ParthenonManager &pman) {
         pman.app_input->boundary_conditions[loc[d - 1][outer]] = reflect[d - 1][outer];
       } else if (bc == "polar") {
         PARTHENON_REQUIRE(d == 2, "Polar boundary conditions only supported in X2!");
-        pman.app_input->boundary_conditions[loc[d-1][outer]] = polar[d-1][outer];
+        pman.app_input->boundary_conditions[loc[d - 1][outer]] = polar[d - 1][outer];
       } else if (bc == "outflow") {
         pman.app_input->boundary_conditions[loc[d - 1][outer]] = outflow[d - 1][outer];
         if (d == 1) {
