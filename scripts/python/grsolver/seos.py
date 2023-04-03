b04f9b4c639c48cea2c61847496730d65b484f8b
mari2895 <mgogilashvili@fsu.edu>
2023-04-03 14:13:08 -0400

    python scripts for generating initial input for phoebus in phoebus units

---
+++ scripts/python/grsolver/seos.py
@@ -0,0 +1,34 @@
+from singularity_eos import StellarCollapse, thermalqs
+import numpy as np
+import math
+import matplotlib.pyplot as pl
+from scipy.optimize import root, bisect
+
+SMALL = 1e-20 # to avoid dividing by zero
+
+
+
+
+def FindTemperature(rho,p,lmbda,tmin,tmax,eos):
+        def f(x):
+            return eos.PressureFromDensityTemperature(rho,x,lmbda)-p
+        sol=bisect(f,tmin,tmax)
+        #T=sol.x[0]
+        T=sol
+        #print('T=',T, 'rho=',rho,'p=',p)
+
+        return T
+
+def CalculateInternalEnergy(rho,ye,p,tmin,tmax,filename):
+    eos1 = StellarCollapse(filename, use_sp5=False, filter_bmod=True)
+    nlambda = eos1.nlambda # get number of elements per lambda
+    lmbda = np.zeros(nlambda, dtype=np.double)    
+    u=np.zeros(len(rho))
+    eps=np.zeros(len(rho))
+    T=np.zeros(len(rho))
+    for i in range(len(rho)):
+        lmbda[0]=ye[i]
+        T[i]=FindTemperature(rho[i],p[i],lmbda,tmin, tmax,eos1)
+        eps[i]=eos1.InternalEnergyFromDensityTemperature(rho[i],T[i],lmbda)
+        u[i]=eps[i]*rho[i]
+    return eps,u, T
