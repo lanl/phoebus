5867fb5b63e1887711b05667387eb5ebc4c27dba
mari2895 <mgogilashvili@fsu.edu>
2023-04-04 13:50:08 -0400

    unit test for the reader

--- src/progenitor/ascii_reader.cpp
+++ src/progenitor/ascii_reader.cpp
@@ -7,32 +7,31 @@
 #include <utils/error_checking.hpp>
 #include "ascii_reader.hpp"
 
-using namespace std;
 namespace AsciiReader {
-void readtable (string filename, std::vector<double> mydata[11]){
-  ifstream myfile(filename.c_str());
- if (!myfile.is_open()){
-   stringstream msg;
-   msg << "file " << filename << " not found." << std::endl;
-   PARTHENON_FAIL(msg);
+  void readtable (std::string filename, std::vector<double> mydata[11]){
+    std::ifstream myfile(filename.c_str());
+    if (!myfile.is_open()){
+      std::stringstream msg;
+      msg << "file " << filename << " not found." << std::endl;
+      PARTHENON_FAIL(msg);
  }
  int i=0;
  while(!myfile.eof()){
-   string line;
+   std::string line;
    std::getline(myfile, line);
    if(line[0] =='#') continue; //for comments
-   stringstream ss(line);
+   std::stringstream ss(line);
    int col = 0;
    double temp;
    while(ss >> temp) {
      mydata[col].push_back(temp);
      col++;
    }
-
  }
 }
- double calculatemass(std::vector<double> mydata[11]){
- double mass=0;
+
+double calculatemass(std::vector<double> mydata[11]){
+  double mass=0;
  for (int i=0; i<=mydata[0].size()-1; i++){
    std::vector<double> &rho = mydata[COLUMNS::rho];
    std::vector<double> &rad = mydata[COLUMNS::rad];
