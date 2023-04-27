4b37c4c4911fe9fe9f47b6e8fc32624472353c8f
Benjamin Ransom Ryan <brryan@lanl.gov>
2023-04-27 13:06:11 -0600

    Add python formatter, reformat everything

--- scripts/python/phoebus_opacities.py
+++ scripts/python/phoebus_opacities.py
@@ -17,50 +17,69 @@ from phoebus_constants import cgs, scalefree
 # ---------------------------------------------------------------------------- #
 # -- Thermal distribution function for neutrinos
 class ThermalDistribution:
-  def __init__(self, constants=cgs, nspecies=3):
-    self.constants = constants
-    self.h = self.constants['HPL']
-    self.kb = self.constants['KBOL']
-    self.c = self.constants['CL']
-    self.nspecies = nspecies
+    def __init__(self, constants=cgs, nspecies=3):
+        self.constants = constants
+        self.h = self.constants["HPL"]
+        self.kb = self.constants["KBOL"]
+        self.c = self.constants["CL"]
+        self.nspecies = nspecies
 
-  def Bnu(self, T, nu):
-    x = self.h*nu/(self.kb*T)
-    return self.nspecies * (2. * self.h * nu**3)/self.c**2/(np.exp(x) + 1.)
+    def Bnu(self, T, nu):
+        x = self.h * nu / (self.kb * T)
+        return (
+            self.nspecies * (2.0 * self.h * nu ** 3) / self.c ** 2 / (np.exp(x) + 1.0)
+        )
 
-  def T_from_J(self, J):
-    return pow(15.*self.c**3*self.h**3*J/(7.*self.kb**4*np.pi**5*self.nspecies),1./4.)
+    def T_from_J(self, J):
+        return pow(
+            15.0
+            * self.c ** 3
+            * self.h ** 3
+            * J
+            / (7.0 * self.kb ** 4 * np.pi ** 5 * self.nspecies),
+            1.0 / 4.0,
+        )
 
-  def J_from_T(self, T):
-    return 7.*self.kb**4*np.pi**5*self.nspecies*T**4/(15.*self.c**3*self.h**3)
+    def J_from_T(self, T):
+        return (
+            7.0
+            * self.kb ** 4
+            * np.pi ** 5
+            * self.nspecies
+            * T ** 4
+            / (15.0 * self.c ** 3 * self.h ** 3)
+        )
+
 
 # ---------------------------------------------------------------------------- #
 # -- Base class containing required data and functions
 class BaseOpacity:
-  def __init__(self, constants='cgs'):
-    self.model = None
-    constants_dict = {'cgs' : cgs, 'scalefree' : scalefree}
-    self.dist = ThermalDistribution(constants_dict[constants])
+    def __init__(self, constants="cgs"):
+        self.model = None
+        constants_dict = {"cgs": cgs, "scalefree": scalefree}
+        self.dist = ThermalDistribution(constants_dict[constants])
 
-  def jnu(self, rho, T, Ye, nu):
-    raise NotImplementedError()
-  def alphanu(self, rho, T, Ye, nu):
-    raise NotImplementedError()
+    def jnu(self, rho, T, Ye, nu):
+        raise NotImplementedError()
+
+    def alphanu(self, rho, T, Ye, nu):
+        raise NotImplementedError()
+
 
 # -- Gray opacity
 class GrayOpacity(BaseOpacity):
-  def __init__(self, params, constants='cgs'):
-    super().__init__(constants)
-    self.model = "gray"
-    self.kappa = params['opacity/gray_kappa']
+    def __init__(self, params, constants="cgs"):
+        super().__init__(constants)
+        self.model = "gray"
+        self.kappa = params["opacity/gray_kappa"]
 
-  def alphanu(self, rho, T, Ye, nu):
-    return rho*self.kappa
+    def alphanu(self, rho, T, Ye, nu):
+        return rho * self.kappa
 
-  def jnu(self, rho, T, Ye, nu):
-    return self.alphanu(rho, T, Ye, nu) * self.dist.Bnu(T, nu)
+    def jnu(self, rho, T, Ye, nu):
+        return self.alphanu(rho, T, Ye, nu) * self.dist.Bnu(T, nu)
 
 
 # ---------------------------------------------------------------------------- #
 # -- Dictionary of opacity types
-opacity_type_dict = {'gray' : GrayOpacity}
+opacity_type_dict = {"gray": GrayOpacity}
--- tst/regression/regression_test.py
+++ tst/regression/regression_test.py
@@ -17,7 +17,8 @@ import numpy as np
 from subprocess import call
 import shutil
 import glob
-sys.path.insert(0,"../../external/parthenon/scripts/python/packages/parthenon_tools")
+
+sys.path.insert(0, "../../external/parthenon/scripts/python/packages/parthenon_tools")
 from parthenon_tools import phdf
 import __main__
 
@@ -25,91 +26,92 @@ import __main__
 # Constants
 #
 
-BUILD_DIR='build'
-RUN_DIR='run'
-SOURCE_DIR='../../../'
-NUM_PROCS=4 # Default values for cmake --build --parallel can overwhelm CI systems
-TEMPORARY_INPUT_FILE='test_input.pin'
-SCRIPT_NAME=os.path.basename(__main__.__file__).split('.py')[0]
+BUILD_DIR = "build"
+RUN_DIR = "run"
+SOURCE_DIR = "../../../"
+NUM_PROCS = 4  # Default values for cmake --build --parallel can overwhelm CI systems
+TEMPORARY_INPUT_FILE = "test_input.pin"
+SCRIPT_NAME = os.path.basename(__main__.__file__).split(".py")[0]
 
 # ------------------------------------------------------------------------------------------------ #
 # Utility functions
 #
 
 # -- Compare two values up to some floating point tolerance
-def soft_equiv(val, ref, tol = 1.e-5):
-  numerator = np.fabs(val - ref)
-  denominator = max(np.fabs(ref), 1.e-10)
+def soft_equiv(val, ref, tol=1.0e-5):
+    numerator = np.fabs(val - ref)
+    denominator = max(np.fabs(ref), 1.0e-10)
 
-  if numerator/denominator > tol:
-    return False
-  else:
-    return True
+    if numerator / denominator > tol:
+        return False
+    else:
+        return True
+
 
 # -- Modify key in input file, add key (and block) if not present, write new file
 def modify_input(dict_key, value, input_file):
-  key = dict_key.split('/')[-1]
-  block = dict_key.split(key)[0][:-1]
+    key = dict_key.split("/")[-1]
+    block = dict_key.split(key)[0][:-1]
 
-  new_input_file = []
+    new_input_file = []
 
-  current_block = None
+    current_block = None
 
-  input_found = False
+    input_found = False
 
-  with open(input_file, 'r') as infile:
-    lines = infile.readlines()
-    for line in lines:
-      sline = line.strip()
+    with open(input_file, "r") as infile:
+        lines = infile.readlines()
+        for line in lines:
+            sline = line.strip()
 
-      # Skip empty lines and comments
-      if len(sline) == 0 or sline[0] == '#':
-        continue
+            # Skip empty lines and comments
+            if len(sline) == 0 or sline[0] == "#":
+                continue
 
-      # Check for block
-      elif sline[0] == '<':
-        current_block = sline.split('<')[1].split('>')[0]
+            # Check for block
+            elif sline[0] == "<":
+                current_block = sline.split("<")[1].split(">")[0]
 
-      # Check for key
-      elif len(sline.split('=')) != 2 or ',' in sline or '&' in sline:
-        # Multiple values not supported for modification
-        new_input_file.append(line)
-        continue
+            # Check for key
+            elif len(sline.split("=")) != 2 or "," in sline or "&" in sline:
+                # Multiple values not supported for modification
+                new_input_file.append(line)
+                continue
 
-      else:
+            else:
 
-        current_key = sline.split('=')[0].strip()
-        current_value = sline.split('=')[1].strip()
+                current_key = sline.split("=")[0].strip()
+                current_value = sline.split("=")[1].strip()
 
-        newline = line
-        if block == current_block and key == current_key:
-          newline = key + ' = ' + str(value) + '\n'
+                newline = line
+                if block == current_block and key == current_key:
+                    newline = key + " = " + str(value) + "\n"
 
-        new_input_file.append(newline)
-        input_found = True
-        continue
+                new_input_file.append(newline)
+                input_found = True
+                continue
 
+            new_input_file.append(line)
 
-      new_input_file.append(line)
+    index = None
+    if input_found == False:
+        print(f'Input "{block}" "{key}" not found!')
+        for i, line in enumerate(new_input_file):
+            if line == f"<{block}>":
+                index = i
 
-  index = None
-  if input_found == False:
-    print(f"Input \"{block}\" \"{key}\" not found!")
-    for i, line in enumerate(new_input_file):
-      if line == f"<{block}>":
-        index = i
+    if index is None:
+        # Block doesn't exist
+        new_input_file.append(f"<{block}>\n")
+        new_input_file.append(key + " = " + str(value) + "\n")
+    else:
+        # Block exists but key doesn't
+        new_input_file.insert(index, key + " = " + str(value) + "\n")
 
-  if index is None:
-    # Block doesn't exist
-    new_input_file.append(f"<{block}>\n")
-    new_input_file.append(key + ' = ' + str(value) + '\n')
-  else:
-    # Block exists but key doesn't
-    new_input_file.insert(index, key + ' = ' + str(value) + '\n')
+    with open(input_file, "w") as outfile:
+        for line in new_input_file:
+            outfile.write(line)
 
-  with open(input_file, 'w') as outfile:
-    for line in new_input_file:
-      outfile.write(line)
 
 # ------------------------------------------------------------------------------------------------ #
 # Common regression test tools
@@ -117,162 +119,189 @@ def modify_input(dict_key, value, input_file):
 
 # -- Configure and build phoebus with problem-specific options
 def build_code(geometry, use_gpu=False, build_type="Release"):
-  if os.path.isdir(BUILD_DIR):
-    print(f"BUILD_DIR \"{BUILD_DIR}\" already exists! Clean up before calling a regression test script!")
-    sys.exit(os.EX_SOFTWARE)
-  os.mkdir(BUILD_DIR)
-  os.chdir(BUILD_DIR)
+    if os.path.isdir(BUILD_DIR):
+        print(
+            f'BUILD_DIR "{BUILD_DIR}" already exists! Clean up before calling a regression test script!'
+        )
+        sys.exit(os.EX_SOFTWARE)
+    os.mkdir(BUILD_DIR)
+    os.chdir(BUILD_DIR)
 
-  # Base configure options
-  configure_options = []
-  if build_type == "Release":
-    configure_options.append("-DCMAKE_BUILD_TYPE=Release")
-  elif build_type == "Debug":
-    configure_options.append("-DCMAKE_BUILD_TYPE=Debug")
-  else:
-    print(f"Build type \"{build_type}\" not known!")
-    sys.exit(os.EX_SOFTWARE)
-  configure_options.append("-DPHOEBUS_ENABLE_UNIT_TESTS=OFF")
-  configure_options.append("-DMAX_NUMBER_CONSERVED_VARS=10")
-  configure_options.append("-DPHOEBUS_CACHE_GEOMETRY=ON")
-  configure_options.append("-DPARTHENON_DISABLE_HDF5_COMPRESSION=ON")
-  if use_gpu:
-    configure_options.append("-DPHOEBUS_ENABLE_CUDA=ON")
-    configure_options.append("-DKokkos_ARCH_VOLTA70=ON")
-    configure_options.append("-DKokkos_ARCH_POWER9=ON")
-    configure_options.append("-DKokkos_ENABLE_CUDA=ON")
-    configure_options.append("-DCMAKE_CXX_COMPILER=/home/brryan/github/phoebus/external/parthenon/external/Kokkos/bin/nvcc_wrapper")
+    # Base configure options
+    configure_options = []
+    if build_type == "Release":
+        configure_options.append("-DCMAKE_BUILD_TYPE=Release")
+    elif build_type == "Debug":
+        configure_options.append("-DCMAKE_BUILD_TYPE=Debug")
+    else:
+        print(f'Build type "{build_type}" not known!')
+        sys.exit(os.EX_SOFTWARE)
+    configure_options.append("-DPHOEBUS_ENABLE_UNIT_TESTS=OFF")
+    configure_options.append("-DMAX_NUMBER_CONSERVED_VARS=10")
+    configure_options.append("-DPHOEBUS_CACHE_GEOMETRY=ON")
+    configure_options.append("-DPARTHENON_DISABLE_HDF5_COMPRESSION=ON")
+    if use_gpu:
+        configure_options.append("-DPHOEBUS_ENABLE_CUDA=ON")
+        configure_options.append("-DKokkos_ARCH_VOLTA70=ON")
+        configure_options.append("-DKokkos_ARCH_POWER9=ON")
+        configure_options.append("-DKokkos_ENABLE_CUDA=ON")
+        configure_options.append(
+            "-DCMAKE_CXX_COMPILER=/home/brryan/github/phoebus/external/parthenon/external/Kokkos/bin/nvcc_wrapper"
+        )
 
-  # Geometry (problem-dependent)
-  configure_options.append(f"-DPHOEBUS_GEOMETRY={geometry}")
+    # Geometry (problem-dependent)
+    configure_options.append(f"-DPHOEBUS_GEOMETRY={geometry}")
 
-  cmake_call = []
-  cmake_call.append('cmake')
-  for option in configure_options:
-    cmake_call.append(option)
-  cmake_call.append(SOURCE_DIR)
+    cmake_call = []
+    cmake_call.append("cmake")
+    for option in configure_options:
+        cmake_call.append(option)
+    cmake_call.append(SOURCE_DIR)
 
-  # Configure
-  call(cmake_call)
+    # Configure
+    call(cmake_call)
 
-  # Compile
-  call(['cmake', '--build', '.', '--parallel', str(NUM_PROCS)])
+    # Compile
+    call(["cmake", "--build", ".", "--parallel", str(NUM_PROCS)])
 
-  # Return to base directory
-  os.chdir('..')
+    # Return to base directory
+    os.chdir("..")
+
 
 # -- Clean up working directory
 def cleanup():
-  if os.getcwd().split(os.sep)[-1] == BUILD_DIR or os.getcwd().split(os.sep)[-1] == RUN_DIR:
-    os.chdir('..')
+    if (
+        os.getcwd().split(os.sep)[-1] == BUILD_DIR
+        or os.getcwd().split(os.sep)[-1] == RUN_DIR
+    ):
+        os.chdir("..")
 
-  if os.path.isabs(BUILD_DIR):
-    print(f"Absolute paths not allowed for build directory \"{BUILD_DIR}\" -- unsafe when cleaning up!")
-    sys.exit(os.EX_SOFTWARE)
+    if os.path.isabs(BUILD_DIR):
+        print(
+            f'Absolute paths not allowed for build directory "{BUILD_DIR}" -- unsafe when cleaning up!'
+        )
+        sys.exit(os.EX_SOFTWARE)
 
-  if os.path.isabs(RUN_DIR):
-    print(f"Absolute paths not allowed for run directory \"{RUN_DIR}\" -- unsafe when cleaning up!")
-    sys.exit(os.EX_SOFTWARE)
+    if os.path.isabs(RUN_DIR):
+        print(
+            f'Absolute paths not allowed for run directory "{RUN_DIR}" -- unsafe when cleaning up!'
+        )
+        sys.exit(os.EX_SOFTWARE)
 
-  if os.path.exists(BUILD_DIR):
-    try:
-      shutil.rmtree(BUILD_DIR)
-    except:
-      print(f"Error cleaning up build directory \"{BUILD_DIR}\"!")
+    if os.path.exists(BUILD_DIR):
+        try:
+            shutil.rmtree(BUILD_DIR)
+        except:
+            print(f'Error cleaning up build directory "{BUILD_DIR}"!')
 
-  if os.path.exists(RUN_DIR):
-    try:
-      shutil.rmtree(RUN_DIR)
-    except:
-      print(f"Error cleaning up build directory \"{RUN_DIR}\"!")
+    if os.path.exists(RUN_DIR):
+        try:
+            shutil.rmtree(RUN_DIR)
+        except:
+            print(f'Error cleaning up build directory "{RUN_DIR}"!')
+
 
 # -- Run test problem with previously built code, input file, and modified inputs, and compare
 #    to gold output
-def gold_comparison(variables, input_file, modified_inputs={},
-                    executable=None, geometry='Minkowski', use_gpu=False, use_mpiexec=False,
-                    build_type='Release', upgold=False, compression_factor=1, tolerance=1.e-5):
+def gold_comparison(
+    variables,
+    input_file,
+    modified_inputs={},
+    executable=None,
+    geometry="Minkowski",
+    use_gpu=False,
+    use_mpiexec=False,
+    build_type="Release",
+    upgold=False,
+    compression_factor=1,
+    tolerance=1.0e-5,
+):
 
-  if executable is None:
-    executable = os.path.join(BUILD_DIR, 'src', 'phoebus')
-    build_code(geometry, use_gpu, build_type)
+    if executable is None:
+        executable = os.path.join(BUILD_DIR, "src", "phoebus")
+        build_code(geometry, use_gpu, build_type)
 
-  if os.path.isdir(RUN_DIR):
-    print(f"RUN_DIR \"{RUN_DIR}\" already exists! Clean up before calling a regression test script!")
-    sys.exit(os.EX_SOFTWARE)
-  os.mkdir(RUN_DIR)
-  os.chdir(RUN_DIR)
+    if os.path.isdir(RUN_DIR):
+        print(
+            f'RUN_DIR "{RUN_DIR}" already exists! Clean up before calling a regression test script!'
+        )
+        sys.exit(os.EX_SOFTWARE)
+    os.mkdir(RUN_DIR)
+    os.chdir(RUN_DIR)
 
-  # Copy test problem and modify inputs
-  shutil.copyfile(input_file, TEMPORARY_INPUT_FILE)
-  for key in modified_inputs:
-    modify_input(key, modified_inputs[key], TEMPORARY_INPUT_FILE)
+    # Copy test problem and modify inputs
+    shutil.copyfile(input_file, TEMPORARY_INPUT_FILE)
+    for key in modified_inputs:
+        modify_input(key, modified_inputs[key], TEMPORARY_INPUT_FILE)
 
-  # Run test problem
-  preamble = []
-  if use_mpiexec:
-      preamble = preamble + ['mpiexec', '-n', '1']
-  if os.path.isabs(executable):
-    call(preamble + [executable, '-i', TEMPORARY_INPUT_FILE])
-  else:
-    call(preamble + [os.path.join('..', executable), '-i', TEMPORARY_INPUT_FILE])
-
-  # Get last dump file
-  dumpfiles = np.sort(glob.glob('*.phdf'))
-  if len(dumpfiles) == 0:
-    print("Could not load any dump files!")
-    sys.exit(os.EX_SOFTWARE)
-  dump = phdf.phdf(dumpfiles[-1])
-
-  # Construct array of results values
-  variables_data = np.empty(shape=(0))
-  for variable_name in variables:
-    variable = dump.Get(variable_name)
-    if len(variable.shape) > 1:
-      dim = variable.shape[0]
-      for d in range(dim):
-        variables_data = np.concatenate((variables_data, variable[d,:]))
+    # Run test problem
+    preamble = []
+    if use_mpiexec:
+        preamble = preamble + ["mpiexec", "-n", "1"]
+    if os.path.isabs(executable):
+        call(preamble + [executable, "-i", TEMPORARY_INPUT_FILE])
     else:
-      variables_data = np.concatenate((variables_data, variable))
+        call(preamble + [os.path.join("..", executable), "-i", TEMPORARY_INPUT_FILE])
 
-  # Compress results, if desired
-  compression_factor = int(compression_factor)
-  compressed_variables = np.zeros(len(variables_data) // compression_factor)
-  for n in range(len(compressed_variables)):
-    compressed_variables[n] = variables_data[compression_factor*n]
-  variables_data = compressed_variables
+    # Get last dump file
+    dumpfiles = np.sort(glob.glob("*.phdf"))
+    if len(dumpfiles) == 0:
+        print("Could not load any dump files!")
+        sys.exit(os.EX_SOFTWARE)
+    dump = phdf.phdf(dumpfiles[-1])
 
-  # Write gold file, or compare to existing gold file
-  success = True
-  gold_name = os.path.join('../', SCRIPT_NAME) + '.gold'
-  if upgold:
-    np.savetxt(gold_name, variables_data, newline='\n')
-  else:
-    gold_variables = np.loadtxt(gold_name)
-    if not len(gold_variables) == len(variables_data):
-      print("Length of gold variables does not match calculated variables!")
-      success = False
+    # Construct array of results values
+    variables_data = np.empty(shape=(0))
+    for variable_name in variables:
+        variable = dump.Get(variable_name)
+        if len(variable.shape) > 1:
+            dim = variable.shape[0]
+            for d in range(dim):
+                variables_data = np.concatenate((variables_data, variable[d, :]))
+        else:
+            variables_data = np.concatenate((variables_data, variable))
+
+    # Compress results, if desired
+    compression_factor = int(compression_factor)
+    compressed_variables = np.zeros(len(variables_data) // compression_factor)
+    for n in range(len(compressed_variables)):
+        compressed_variables[n] = variables_data[compression_factor * n]
+    variables_data = compressed_variables
+
+    # Write gold file, or compare to existing gold file
+    success = True
+    gold_name = os.path.join("../", SCRIPT_NAME) + ".gold"
+    if upgold:
+        np.savetxt(gold_name, variables_data, newline="\n")
     else:
-      for n in range(len(gold_variables)):
-        if not soft_equiv(variables_data[n], gold_variables[n], tol=tolerance):
-          success = False
+        gold_variables = np.loadtxt(gold_name)
+        if not len(gold_variables) == len(variables_data):
+            print("Length of gold variables does not match calculated variables!")
+            success = False
+        else:
+            for n in range(len(gold_variables)):
+                if not soft_equiv(variables_data[n], gold_variables[n], tol=tolerance):
+                    success = False
 
-  cleanup()
+    cleanup()
 
-  # Report upgolding, success, or failure
-  if upgold:
-    print(f"Gold file {gold_name} updated!")
-  else:
-    if success:
-      print("TEST PASSED")
-      return os.EX_OK
+    # Report upgolding, success, or failure
+    if upgold:
+        print(f"Gold file {gold_name} updated!")
     else:
-      print("TEST FAILED")
-      mean_error = np.mean(variables_data - gold_variables)
-      max_error = np.max(np.fabs(variables_data - gold_variables))
-      max_frac_error = np.max(np.fabs(variables_data - gold_variables)/
-                              np.clip(np.fabs(gold_variables), 1.e-100, None))
+        if success:
+            print("TEST PASSED")
+            return os.EX_OK
+        else:
+            print("TEST FAILED")
+            mean_error = np.mean(variables_data - gold_variables)
+            max_error = np.max(np.fabs(variables_data - gold_variables))
+            max_frac_error = np.max(
+                np.fabs(variables_data - gold_variables)
+                / np.clip(np.fabs(gold_variables), 1.0e-100, None)
+            )
 
-      print(f"Mean error:           {mean_error}")
-      print(f"Max error:            {max_error}")
-      print(f"Max fractional error: {max_frac_error}")
-      return os.EX_SOFTWARE
+            print(f"Mean error:           {mean_error}")
+            print(f"Max error:            {max_error}")
+            print(f"Max fractional error: {max_frac_error}")
+            return os.EX_SOFTWARE
