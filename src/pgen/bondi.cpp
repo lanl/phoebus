2d29855a515cd6ff8171459a89e508869e947913
Alexander Dittmann <dittmann@astro.umd.edu>
2023-09-25 19:22:28 -0400

    floor/zero near-horizon quantities, can now set a>0

--- src/pgen/bondi.cpp
+++ src/pgen/bondi.cpp
@@ -11,10 +11,28 @@
 // distribute copies to the public, perform publicly and display
 // publicly, and to permit others to do so.
 
+//#include "geometry/boyer_lindquist.hpp"
+//#include "geometry/mckinney_gammie_ryan.hpp"
+//#include "pgen/pgen.hpp"
+//#include "utils/error_checking.hpp"
+
+// Parthenon
+#include <globals.hpp>
+
+// Phoebus
+#include "fluid/con2prim_robust.hpp"
 #include "geometry/boyer_lindquist.hpp"
 #include "geometry/mckinney_gammie_ryan.hpp"
 #include "pgen/pgen.hpp"
-#include "utils/error_checking.hpp"
+#include "phoebus_utils/adiabats.hpp"
+#include "phoebus_utils/reduction.hpp"
+#include "phoebus_utils/relativity_utils.hpp"
+#include "phoebus_utils/robust.hpp"
+#include "phoebus_utils/root_find.hpp"
+#include "phoebus_utils/unit_conversions.hpp"
+#include "radiation/radiation.hpp"
+
+
 
 // namespace phoebus {
 
@@ -139,6 +157,7 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
   const Real a = pin->GetReal("geometry", "a");
   auto bl = Geometry::BoyerLindquist(a);
 
+  auto floor = pmb->packages.Get("fixup")->Param<fixup::Floors>("floor");
   auto geom = Geometry::GetCoordinateSystem(rc);
 
   // set up transformation stuff
@@ -158,6 +177,7 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
         const Real x2 = coords.Xc<2>(k, j, i);
         const Real x3 = coords.Xc<3>(k, j, i);
 
+
         Real r = tr.bl_radius(x1);
         while (r < Rhor) {
           x1 += coords.Dxc<1>(i);
@@ -170,56 +190,71 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
           eos_lambda[0] = v(iye, k, j, i);
         }
 
-        v(itmp, k, j, i) = get_bondi_temp(r, n, C1, C2, Tc, rs);
-        v(irho, k, j, i) = std::pow(v(itmp, k, j, i), n);
-        v(ieng, k, j, i) = v(irho, k, j, i) * v(itmp, k, j, i) / (gam - 1.0);
-        v(iprs, k, j, i) = eos.PressureFromDensityInternalEnergy(
-            v(irho, k, j, i), v(ieng, k, j, i) / v(irho, k, j, i), eos_lambda);
-        v(igm1, k, j, i) = eos.BulkModulusFromDensityTemperature(
-                               v(irho, k, j, i), v(itmp, k, j, i), eos_lambda) /
-                           v(iprs, k, j, i);
-        Real ucon_bl[] = {0.0, 0.0, 0.0, 0.0};
-        ucon_bl[1] = -C1 / (std::pow(v(itmp, k, j, i), n) * std::pow(r, 2));
+        if (r > 5*Rhor) {
+          v(itmp, k, j, i) = get_bondi_temp(r, n, C1, C2, Tc, rs);
+          v(irho, k, j, i) = std::pow(v(itmp, k, j, i), n);
+          v(ieng, k, j, i) = v(irho, k, j, i) * v(itmp, k, j, i) / (gam - 1.0);
+          v(iprs, k, j, i) = eos.PressureFromDensityInternalEnergy(
+              v(irho, k, j, i), v(ieng, k, j, i) / v(irho, k, j, i), eos_lambda);
+          v(igm1, k, j, i) = eos.BulkModulusFromDensityTemperature(
+                                 v(irho, k, j, i), v(itmp, k, j, i), eos_lambda) /
+                             v(iprs, k, j, i);
+          Real ucon_bl[] = {0.0, 0.0, 0.0, 0.0};
+          ucon_bl[1] = -C1 / (std::pow(v(itmp, k, j, i), n) * std::pow(r, 2));
 
-        Real gcov[4][4];
-        const Real th = tr.bl_theta(x1, x2);
-        bl.SpacetimeMetric(0.0, r, th, x3, gcov);
-        Real AA = gcov[0][0];
-        Real BB = 2. * (gcov[0][1] * ucon_bl[1] + gcov[0][2] * ucon_bl[2] +
-                        gcov[0][3] * ucon_bl[3]);
-        Real CC = 1. + gcov[1][1] * ucon_bl[1] * ucon_bl[1] +
-                  gcov[2][2] * ucon_bl[2] * ucon_bl[2] +
-                  gcov[3][3] * ucon_bl[3] * ucon_bl[3] +
-                  2. * (gcov[1][2] * ucon_bl[1] * ucon_bl[2] +
-                        gcov[1][3] * ucon_bl[1] * ucon_bl[3] +
-                        gcov[2][3] * ucon_bl[2] * ucon_bl[3]);
-        Real discr = BB * BB - 4. * AA * CC;
-        ucon_bl[0] = (-BB - std::sqrt(discr)) / (2. * AA);
-        const Real W_bl = ucon_bl[0] * bl.Lapse(0.0, r, th, x3);
+          Real gcov[4][4];
+          const Real th = tr.bl_theta(x1, x2);
+          bl.SpacetimeMetric(0.0, r, th, x3, gcov);
+          Real AA = gcov[0][0];
+          Real BB = 2. * (gcov[0][1] * ucon_bl[1] + gcov[0][2] * ucon_bl[2] +
+                          gcov[0][3] * ucon_bl[3]);
+          Real CC = 1. + gcov[1][1] * ucon_bl[1] * ucon_bl[1] +
+                    gcov[2][2] * ucon_bl[2] * ucon_bl[2] +
+                    gcov[3][3] * ucon_bl[3] * ucon_bl[3] +
+                    2. * (gcov[1][2] * ucon_bl[1] * ucon_bl[2] +
+                          gcov[1][3] * ucon_bl[1] * ucon_bl[3] +
+                          gcov[2][3] * ucon_bl[2] * ucon_bl[3]);
+          Real discr = BB * BB - 4. * AA * CC;
+          ucon_bl[0] = (-BB - std::sqrt(discr)) / (2. * AA);
+          const Real W_bl = ucon_bl[0] * bl.Lapse(0.0, r, th, x3);
 
-        Real ucon[4];
-        tr.bl_to_fmks(x1, x2, x3, a, ucon_bl, ucon);
+          Real ucon[4];
+          tr.bl_to_fmks(x1, x2, x3, a, ucon_bl, ucon);
 
-        // ucon won't be properly normalized here if x1 is not consistent with i
-        // so renormalize
-        geom.SpacetimeMetric(CellLocation::Cent, k, j, i, gcov);
-        AA = gcov[0][0];
-        BB = 2. * (gcov[0][1] * ucon[1] + gcov[0][2] * ucon[2] + gcov[0][3] * ucon[3]);
-        CC = 1. + gcov[1][1] * ucon[1] * ucon[1] + gcov[2][2] * ucon[2] * ucon[2] +
-             gcov[3][3] * ucon[3] * ucon[3] +
-             2. * (gcov[1][2] * ucon[1] * ucon[2] + gcov[1][3] * ucon[1] * ucon[3] +
-                   gcov[2][3] * ucon[2] * ucon[3]);
-        discr = BB * BB - 4. * AA * CC;
-        PARTHENON_REQUIRE(discr > 0, "discr < 0");
-        ucon[0] = (-BB - std::sqrt(discr)) / (2. * AA);
+          // ucon won't be properly normalized here if x1 is not consistent with i
+          // so renormalize
+          geom.SpacetimeMetric(CellLocation::Cent, k, j, i, gcov);
+          AA = gcov[0][0];
+          BB = 2. * (gcov[0][1] * ucon[1] + gcov[0][2] * ucon[2] + gcov[0][3] * ucon[3]);
+          CC = 1. + gcov[1][1] * ucon[1] * ucon[1] + gcov[2][2] * ucon[2] * ucon[2] +
+               gcov[3][3] * ucon[3] * ucon[3] +
+               2. * (gcov[1][2] * ucon[1] * ucon[2] + gcov[1][3] * ucon[1] * ucon[3] +
+                     gcov[2][3] * ucon[2] * ucon[3]);
+          discr = BB * BB - 4. * AA * CC;
+          PARTHENON_REQUIRE(discr > 0, "discr < 0");
+          ucon[0] = (-BB - std::sqrt(discr)) / (2. * AA);
 
-        // now get three velocity
-        const Real lapse = geom.Lapse(CellLocation::Cent, k, j, i);
-        Real beta[3];
-        geom.ContravariantShift(CellLocation::Cent, k, j, i, beta);
-        Real W = lapse * ucon[0];
-        for (int d = 0; d < 3; d++) {
-          v(ivlo + d, k, j, i) = ucon[d + 1] + W * beta[d] / lapse;
+          // now get three velocity
+          const Real lapse = geom.Lapse(CellLocation::Cent, k, j, i);
+          Real beta[3];
+          geom.ContravariantShift(CellLocation::Cent, k, j, i, beta);
+          Real W = lapse * ucon[0];
+          for (int d = 0; d < 3; d++) {
+            v(ivlo + d, k, j, i) = ucon[d + 1] + W * beta[d] / lapse;
+          }
+        }
+        else {
+          Real rhoflr = 0.0;
+          Real epsflr;
+          floor.GetFloors(x1, x2, x3, rhoflr, epsflr);
+          for (int d = 0; d < 3; d++) {
+            v(ivlo + d, k, j, i) = 0.0;
+          }
+          v(itmp, k, j, i) = get_bondi_temp(r, n, C1, C2, Tc, rs);
+          v(irho, k, j, i) = rhoflr;
+          v(ieng, k, j, i) = epsflr;
+          v(iprs, k, j, i) = eos.PressureFromDensityInternalEnergy( v(irho, k, j, i), v(ieng, k, j, i) / v(irho, k, j, i), eos_lambda);
+          v(igm1, k, j, i) = eos.BulkModulusFromDensityTemperature( v(irho, k, j, i), v(itmp, k, j, i), eos_lambda) / v(iprs, k, j, i);
         }
       });
 
