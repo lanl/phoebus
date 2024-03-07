fcb26d0878d85bbe627c76ab66a0c1f584296daa
mari2895 <mgogilashvili@fsu.edu>
2024-03-07 17:23:43 -0500

    check for sigma added

--- src/analysis/analysis.cpp
+++ src/analysis/analysis.cpp
@@ -6,6 +6,9 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
   auto analysis_pkg = std::make_shared<StateDescriptor>("analysis");
   Params &params = analysis_pkg->AllParams();
   Real sigma = pin->GetOrAddReal("analysis", "sigma", 2e-3);
+  if (sigma < 0){
+    PARTHENON_THROW("sigma must be greater than 0");
+  }
   params.Add("sigma", sigma);
 
   return analysis_pkg;
