6c12f3c464fc2d598ab7e5047c3b1fbd973b35f2
Jonah Maxwell Miller <jonahm@lanl.gov>
2024-03-07 15:26:19 -0700

    formatting

--- src/analysis/analysis.cpp
+++ src/analysis/analysis.cpp
@@ -6,7 +6,7 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
   auto analysis_pkg = std::make_shared<StateDescriptor>("analysis");
   Params &params = analysis_pkg->AllParams();
   Real sigma = pin->GetOrAddReal("analysis", "sigma", 2e-3);
-  if (sigma < 0){
+  if (sigma < 0) {
     PARTHENON_THROW("sigma must be greater than 0");
   }
   params.Add("sigma", sigma);
