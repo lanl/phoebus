87e0ba69f6319f6819c377ba75763284336563f5
Jonah Maxwell Miller <jonahm@lanl.gov>
2024-03-07 15:10:21 -0700

    formatting

--- src/analysis/analysis.cpp
+++ src/analysis/analysis.cpp
@@ -5,7 +5,7 @@ namespace analysis{
 std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
   auto analysis_pkg = std::make_shared<StateDescriptor>("analysis");
   Params &params = analysis_pkg->AllParams();
-  Real sigma = pin->GetOrAddReal("analysis","sigma", 2e-3);
+  Real sigma = pin->GetOrAddReal("analysis", "sigma", 2e-3);
   params.Add("sigma", sigma);
 
   return analysis_pkg;
