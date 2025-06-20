565c5e1d32cb4383b7699ce7cbbbd732de5ee805
Jeremiah Murphy <miah@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-06-20 19:47:14 +0000

    adding Dump3D class and starting capability to PlotScatter3DvsRadius

--- scripts/python/phoebuspy.py
+++ scripts/python/phoebuspy.py
@@ -40,6 +40,58 @@ class Dump1D:
                 pass
         return
 
+class Dump3D:
+    #So far this, the only difference between this and Dump1D is
+    #the size of var[key].  They are inherently 1D in Dump1D and
+    #3D in Dump3D.  Maybe we can unify this into one class at some point.
+    def __init__(self, filename,extractvars=None):
+        with h5py.File(filename, "r") as f:
+            #The various variables and states are embedded within several datasets.  The following print commands show the various keys.
+            #print(f.keys())
+            #print(f["Info"].attrs.keys())
+            #print(f["Params"].attrs.keys())
+            #print(f["Locations"].keys())
+            self.nx,self.ny,self.nz = f["Info"].attrs["MeshBlockSize"]
+            self.NumMB = f["Info"].attrs["NumMeshBlocks"]
+            if (extractvars is None):
+                self.varkeys = ['p.density']
+            elif (extractvars == 'all'):
+                self.varkeys = list(f.keys())[8:]
+            else:
+                self.varkeys = extractvars
+            self.t = f["Info"].attrs["Time"]
+            self.xf = f["Locations/x"][:,:]
+            self.xc = 0.5 * (self.xf[:,1:] + self.xf[:,:-1])
+            self.yf = f["Locations/y"][:,:]
+            self.yc = 0.5 * (self.yf[:,1:] + self.yf[:,:-1])
+            self.zf = f["Locations/z"][:,:]
+            self.zc = 0.5 * (self.zf[:,1:] + self.zf[:,:-1])
+            #Making xgrid,ygrid,zgrid
+            X = self.xc[:, np.newaxis, np.newaxis, :]  # shape: (Nb, 1, 1, Nx)
+            Y = self.yc[:, np.newaxis, :, np.newaxis]  # shape: (Nb, 1, Ny, 1)
+            Z = self.zc[:, :, np.newaxis, np.newaxis]  # shape: (Nb, Nz, 1, 1)
+            # Now broadcast to (Nb, Nz, Ny, Nx)
+            self.xgrid = np.broadcast_to(X, (self.NumMB, self.nz, self.ny, self.nx))
+            self.ygrid = np.broadcast_to(Y, (self.NumMB, self.nz, self.ny, self.nx))
+            self.zgrid = np.broadcast_to(Z, (self.NumMB, self.nz, self.ny, self.nx))
+            self.var = {}
+            for key in self.varkeys:
+                print(f"Reading {key} from {filename}...",end="")
+                self.var[key] = f[key][:,:,:,:] #ib,iz,iy,ix
+                print(f"Done.")
+            try:
+                self.var['monopole_gr/lapse_h'] = f["Params"].attrs['monopole_gr/lapse_h']
+                self.var['monopole_gr/hypersurface_h']=f["Params"].attrs["monopole_gr/hypersurface_h"]
+                self.var['monopole_gr/shift']=f["Params"].attrs["monopole_gr/shift"]
+                self.var['monopole_gr/rhoadm'] = f["Params"].attrs['monopole_gr/matter_h'][0,:]
+                nxgr = np.size(self.var['monopole_gr/lapse_h'])
+                rout = f["Params"].attrs['monopole_gr/rout']
+                rin = f["Params"].attrs['monopole_gr/rin']
+                self.var['monopole_gr/radius'] = np.linspace(rin,rout,num=nxgr)
+            except:
+                pass
+        return
+
 class DumpGR:
     def __init__(self, filename):
         with h5py.File(filename, "r") as f:
@@ -129,6 +181,28 @@ def MovieGR(data,varname='monopole_gr/rhoadm',anax=None,anay=None,ylim=None,xlim
     system(f"ffmpeg -r 10 -f image2 -i img%04d.png -vcodec mpeg2video -crf 25 -pix_fmt yuv420p movieGR.{mvname}.mpeg")
     system("rm img????.png")
     
+    return
+
+def PlotScatter3DvsRadius(ax,data,varname=None):
+    if (varname is None):
+        varname = 'p.density'
+
+    print("Calculating Radius...",end="")
+    radius = np.sqrt(data.xgrid**2 + data.ygrid**2 + data.zgrid**2)
+    print("Done.")
+    nradbins = 100
+    radedges = np.linspace(0.,np.max(radius),nradbins+1)
+    varpercentiles = np.zeros((3,nradbins))
+    #Consider reducing the complexity by looping over meshblocks.
+    for ibin in range(nradbins):
+        idx = np.where((radius > radedges[ibin]) & (radius < radedges[ibin+1]))[0]
+        ninbin = np.size(idx)
+        print(ninbin)
+    exit()
+    print("Plotting Scatter Plot...",end="")
+    ax.scatter(radius,data.var[varname])
+    print("Done.")
+        
     return
 
 def ReadHistory(fname=None):
